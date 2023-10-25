83d4c3f175de321abbecb1bce66deb9aea3ad55a
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-10-25 11:18:06 +0530

    25. 779. K-th Symbol in Grammar.js

--- LeetCode/2023 - October - 30 Day LeetCode Challenge/Week 4: October 22nd - October 28th/25. 779. K-th Symbol in Grammar.js
+++ LeetCode/2023 - October - 30 Day LeetCode Challenge/Week 4: October 22nd - October 28th/25. 779. K-th Symbol in Grammar.js
@@ -43,63 +43,34 @@ row 2: 01
 */
 
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-function largestValues(root) {
-    if (!root) return [];
-    
-    const result = [];
-    const queue = [root];
-    
-    while (queue.length) {
-        let curr_level_size = queue.length;
-        let max_val = Number.MIN_SAFE_INTEGER;
-        
-        for (let i = 0; i < curr_level_size; i++) {
-            const node = queue.shift();
-            max_val = Math.max(max_val, node.val);
-            
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
-        }
-        
-        result.push(max_val);
-    }
-    
-    return result;
-}
+var kthGrammar = function(n, k) {
+    if (n === 1) return 0;
+    let length = 1 << (n - 2);
+    if (k <= length) return kthGrammar(n - 1, k);
+    else return 1 - kthGrammar(n - 1, k - length);
+};
 
 
 /*
 
+# **Solution 1: Recursive Approach**
 
-# **Intuition**
+## **Intuition**
 
-When presented with this problem, the immediate thought is to traverse through the tree level by level. This is because we need to determine the maximum value in each row. Level order traversal, also known as breadth-first search (BFS) for trees, is the ideal strategy for this. Using a queue, we can explore each level of the tree one by one and capture the maximum value for each.
+The initial thought on solving the problem revolves around understanding the pattern of sequence generation as described in the problem statement. As the sequence grows by replacing 0 with 01 and 1 with 10, we can observe a recursive relationship between the nth row and the (n-1)th row.
 
-# **Approach**
+## **Approach**
 
-1. **Initialization**: Begin with initializing a queue and adding the root node to it. This queue will help in level order traversal.
-2. **Level-wise Traversal**: As long as the queue is not empty, we keep on processing nodes. For each level, we will calculate its size (i.e., the number of nodes in the current level). This helps in segregating nodes of different levels.
-3. **Capture Maximum**: For each level, initialize a variable `max_val` with the smallest possible integer. As we process each node in the current level, we update `max_val` to be the maximum between the node's value and the current `max_val`.
-4. **Child Processing**: After processing a node, we add its left and right children (if they exist) to the queue for the next level's processing.
-5. **Result Update**: Once all nodes of a level are processed, the maximum value for that level (`max_val`) is added to the result list.
+Our approach is to leverage the recursive pattern observed. The length of the nth row is double the length of the (n-1)th row. If k is in the first half of the nth row, the symbol at position k in the nth row is the same as the symbol at position k in the (n-1)th row. If k is in the second half of the nth row, the symbol at position k in the nth row is the opposite of the symbol at position (k - length of (n-1)th row) in the (n-1)th row.
 
 
+## **Complexity**
 
-# **Complexity**
-
-- **Time complexity**: O(n)O(n)*O*(*n*). Each node is processed once, and the maximum value at each level is determined in constant time. So, the overall time taken is linear with respect to the number of nodes.
-- **Space complexity**: O(n)O(n)*O*(*n*). In the worst case, the queue can hold all leaf nodes. In a balanced binary tree, the last level can have n/2n/2*n2 nodes. Thus, the space complexity is linear.
-
+- Time complexity: The time complexity is O(n) as there is a single recursive call for every level of n.
+- Space complexity: The space complexity is also O(n) due to the stack space used by the recursive calls.
 
 */
