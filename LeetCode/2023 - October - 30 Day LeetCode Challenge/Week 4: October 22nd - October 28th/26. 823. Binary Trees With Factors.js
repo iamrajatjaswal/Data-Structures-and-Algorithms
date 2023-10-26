25490144b7e0659cbb00ef501c7e1297557106ef
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-10-26 10:38:40 +0530

    UPDATE 26. 823. Binary Trees With Factors.js :: Added relevent information

--- LeetCode/2023 - October - 30 Day LeetCode Challenge/Week 4: October 22nd - October 28th/26. 823. Binary Trees With Factors.js
+++ LeetCode/2023 - October - 30 Day LeetCode Challenge/Week 4: October 22nd - October 28th/26. 823. Binary Trees With Factors.js
@@ -1,5 +1,11 @@
 /*
 
+URL: https://leetcode.com/problems/binary-trees-with-factors
+Difficulty: Medium
+
+
+
+*Problem:*
 Given an array of unique integers, arr, where each integer arr[i] is strictly greater than 1.
 
 We make a binary tree using these integers, and each number may be used for any number of times. Each non-leaf node's value should be equal to the product of the values of its children.
