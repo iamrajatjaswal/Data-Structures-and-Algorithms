340644c1a8d9dc7d529d48f776b91914a7f81deb
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-04 19:13:25 +0530

    UPDATE : 9.while-loops.cpp

---
+++ Striver-A2Z/C++/step-1/step-1.1-things-to-know-in-c++-java-python-or-any-language/9.while-loops.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  // While Loop
+  // int i = 1;
+  // while (i <= 5) {
+  //   cout << "Rajat " << i << endl;
+  //   i = i + 1;
+  // }
+
+  // do while loop
+  int i = 2;
+  do {
+    cout << "Rajat " << i << endl;
+    i = i + 1;
+  } while (i <= 1);
+
+  return 0;
+}
