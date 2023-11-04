7c1bd8eecd5544fdb582cad6875c3814433fd477
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-04 12:05:51 +0530

    CREATE step1.1 -> 2.data-type.cpp

---
+++ Striver-A2Z/C++/step-1/step-1.1-things-to-know-in-c++-java-python-or-any-language/2.data-types.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  /*
+    Datatype: int
+    Size: 4 bytes
+    Range of Values: -2,147,483,648 to 2,147,483,647
+  */
+  int x = 10;
+
+  /*
+    Datatype: long
+    Size: 4 bytes
+    Range of Values: -2,147,483,648 to 2,147,483,647
+  */
+  long y = 15;
+
+  /*
+    Datatype: long long
+    Size: 8 bytes
+    Range of Values: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+  */
+  long long z = 1500000;
+
+  // float, double, long double
+  float a = 5.6;
+  float b = 5;
+  // cout << "Value of y: " << y << endl;
+
+  // sting, getline
+  string s1;
+  string s2;
+  cin >> s1 >> s2;
+  cout << "String 1: " << s1 << " and String 2: " << s2;
+
+  return 0;
+}
