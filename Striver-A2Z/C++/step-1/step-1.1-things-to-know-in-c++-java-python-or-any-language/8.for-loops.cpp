54db864e6a3d48f5a540d14c02c2e6b12f7db0e0
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-04 19:12:27 +0530

    CREATE : 8.for-loops.cpp

---
+++ Striver-A2Z/C++/step-1/step-1.1-things-to-know-in-c++-java-python-or-any-language/8.for-loops.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  // cout << "RJ" << endl;
+  // cout << "RJ" << endl;
+  // cout << "RJ" << endl;
+  // cout << "RJ" << endl;
+  // cout << "RJ" << endl;
+  // without for loop if I want to print this line 10 times then I have to write this line 10 times.
+
+  for (int i = 0; i < 10; i = i + 1) {
+    cout << "RJ" << endl;
+  }
+
+  return 0;
+}
