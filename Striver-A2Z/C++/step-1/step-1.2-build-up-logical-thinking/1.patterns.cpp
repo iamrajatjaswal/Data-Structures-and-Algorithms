8cc38e7f20e45d18274d6738e862dfa4b30f5c14
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-06 00:36:11 +0530

    UPDATE : 1.patterns.cpp in stiver's A2Z Course

--- Striver-A2Z/C++/step-1/step-1.2-build-up-logical-thinking/1.patterns.cpp
+++ Striver-A2Z/C++/step-1/step-1.2-build-up-logical-thinking/1.patterns.cpp
@@ -31,7 +31,7 @@ void print2(int n) {
 
 /**
  * @brief Right Angled Numbered Pyramid I
- * 
+ *
  * @param n
  */
 void print3(int n) {
@@ -45,8 +45,8 @@ void print3(int n) {
 
 /**
  * @brief Right Angled Numbered Pyramid II
- * 
- * @param n 
+ *
+ * @param n
  */
 void print4(int n) {
   for (int i = 1; i <= n; i++) {
@@ -55,7 +55,164 @@ void print4(int n) {
     }
     cout << endl;
   }
-  
+}
+
+/**
+ * @brief Inverted Right Pyramid
+ * In Reverse Loop
+ *
+ * @param n
+ */
+// void print5(int n) {
+//   for (int i = n; i >= 1; i--) {
+//     for (int j = 1; j <= i; j++) {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+/**
+ * @brief Inverted Right Pyramid
+ * In Reverse Loop
+ *
+ * @param n
+ */
+void print5(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n - i + 1; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Inverted Numbered Right Pyramid
+ *
+ * @param n
+ */
+void print6(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n - i + 1; j++) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Star Pyramid
+ *
+ * @param n
+ */
+void print7(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n - i; j++) {
+      cout << "  ";
+    }
+
+    for (int j = 1; j <= 2 * i - 1; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Inverted Star Pyramid
+ *
+ * @param n
+ */
+// Method 1:
+// void print8(int n) {
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= i - 1; j++) {
+//       cout << "  ";
+//     }
+
+//     for (int j = 1; j <= 2 * (n - i + 1) - 1; j++) {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// Method 2:
+void print8(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << "  ";
+    }
+
+    for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Diamond Star Pattern
+ *
+ * @param n
+ */
+void print9(int n) {
+  print7(n);
+  print8(n);
+}
+
+/**
+ * @brief Half Diamond Star Pattern
+ *
+ * @param n
+ */
+void print10(int n) {
+  for (int i = 1; i <= 2 * n - 1; i++) {
+    int stars = i <= n ? i : 2 * n - i;
+    for (int j = 1; j <= stars; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Binary Number Triangle Pattern
+ *
+ * @param n
+ */
+void print11(int n) {
+  for (int i = 1; i <= n; i++) {
+    int start = (i % 2 == 0) ? 0 : 1;
+    for (int j = 1; j <= i; j++) {
+      cout << start << " ";
+      start = 1 - start;
+    }
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Number Crown Pattern
+ *
+ * @param n
+ */
+void print12(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << j << " ";
+    }
+
+    for (int j = 1; j <= 2 * (n - i); j++) {
+      cout << "  ";
+    }
+
+    for (int j = i; j >= 1; j--) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
 }
 
 int main() {
@@ -68,7 +225,16 @@ int main() {
     // print1(n);
     // print2(n);
     // print3(n);
-    print4(n);
+    // print4(n);
+    // print5(n);
+    // print6(n);
+    // print7(n);
+    // print8(n);
+    // print9(n);
+    // print10(n);
+    // print11(n);
+    print12(n);
+    cout << endl;
   }
 
   return 0;
