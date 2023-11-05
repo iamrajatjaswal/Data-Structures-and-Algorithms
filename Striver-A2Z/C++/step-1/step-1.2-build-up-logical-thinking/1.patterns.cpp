15c8c6a2c376ec7bcc51f9c8b7ad359d9aa413bd
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-06 01:24:22 +0530

    UPDATE : 1.patterns.cpp :: Done patterns till 17

--- Striver-A2Z/C++/step-1/step-1.2-build-up-logical-thinking/1.patterns.cpp
+++ Striver-A2Z/C++/step-1/step-1.2-build-up-logical-thinking/1.patterns.cpp
@@ -215,6 +215,79 @@ void print12(int n) {
   }
 }
 
+/**
+ * @brief Increasing Number Triangle pattern
+ *
+ * @param n
+ */
+void print13(int n) {
+  int num = 1;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << num++ << " ";
+    }
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Increasing Letter Triangle Pattern
+ *
+ * @param n
+ */
+void print14(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (char ch = 'A'; ch <= 'A' + i - 1; ch++) {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+
+void print15(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (char ch = 'A'; ch <= 'A' + (n - i); ch++) {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+
+void print16(int n) {
+  char ch = 'A';
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << ch << " ";
+    }
+    ch++;
+    cout << endl;
+  }
+}
+
+/**
+ * @brief Alpha-Hill Pattern
+ *
+ * @param n
+ */
+void print17(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n - i; j++) {
+      cout << "  ";
+    }
+
+    char ch = 'A';
+    int breakpoint = (2 * i + 1) / 2;
+    for (int j = 1; j <= 2 * i - 1; j++) {
+      if (j < breakpoint) {
+        cout << ch++ << " ";
+      } else {
+        cout << ch-- << " ";
+      }
+    }
+    cout << endl;
+  }
+}
+
 int main() {
   int t;
   cin >> t;
@@ -233,7 +306,12 @@ int main() {
     // print9(n);
     // print10(n);
     // print11(n);
-    print12(n);
+    // print12(n);
+    // print13(n);
+    // print14(n);
+    // print15(n);
+    // print16(n);
+    print17(n);
     cout << endl;
   }
 
