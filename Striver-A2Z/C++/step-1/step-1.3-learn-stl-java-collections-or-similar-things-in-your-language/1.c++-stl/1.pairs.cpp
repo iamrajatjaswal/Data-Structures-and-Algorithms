fe502f8afdd506903e8dda4ace67b7556f66afb2
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-06 11:17:00 +0530

    UPDATE : 1.pairs.cpp

--- Striver-A2Z/C++/step-1/step-1.3-learn-stl-java-collections-or-similar-things-in-your-language/1.c++-stl/1.pairs.cpp
+++ Striver-A2Z/C++/step-1/step-1.3-learn-stl-java-collections-or-similar-things-in-your-language/1.c++-stl/1.pairs.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Pairs
+/**
+ * @brief Pairs
+ * If you have a couple of values, you can 
+ * then you can store them in pairs
+ * 
+ * If you have more than two values then you
+ * can store in the nested pairs
+ * 
+ * Also Pairs can also be the datatype of the 
+ * array exactly like other datatypes like int
+ * etc
+ * 
+ */
 void explainPairs() {
   pair<int, int> p = {1, 3};
   cout << p.first << " " << p.second << endl;
