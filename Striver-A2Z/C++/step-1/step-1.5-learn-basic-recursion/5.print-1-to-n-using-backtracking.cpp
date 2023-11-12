622591178a8ed8f08eaf00ffee0da87866470295
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-12 23:21:11 +0530

    CREATE 5.print-1-to-n-using-backtracking.cpp

---
+++ Striver-A2Z/C++/step-1/step-1.5-learn-basic-recursion/5.print-1-to-n-using-backtracking.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i, int n) {
+  if (i < 1)
+    return;
+
+  f(i - 1, n);
+
+  cout << i << endl;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  f(n, n);
+
+  return 0;
+}
