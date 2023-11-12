27e99fcf7355fa27d3d97ba6cd90198d9755046d
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-13 00:11:21 +0530

    CREATE 11.reverse-an-array.cpp

---
+++ Striver-A2Z/C++/step-1/step-1.5-learn-basic-recursion/11.reverse-an-array.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseArray(int i, int arr[], int n) {
+  if (i >= n / 2)
+    return;
+
+  swap(arr[i], arr[n - i - 1]);
+
+  reverseArray(i + 1, arr, n);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int arr[n];
+
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  reverseArray(0, arr, n);
+
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << endl;
+
+  return 0;
+}
