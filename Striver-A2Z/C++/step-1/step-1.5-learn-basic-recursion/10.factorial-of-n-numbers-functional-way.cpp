7c5169acd7f4b20e2c340acdc0548f9252fab06a
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-12 23:49:30 +0530

    CREATE 10.factorial-of-n-numbers-functional-way.cpp

---
+++ Striver-A2Z/C++/step-1/step-1.5-learn-basic-recursion/10.factorial-of-n-numbers-functional-way.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n) {
+  if (n <= 0)
+    return 1;
+
+  return n * factorial(n - 1);
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  int output = factorial(n);
+  cout << output;
+
+  return 0;
+}
