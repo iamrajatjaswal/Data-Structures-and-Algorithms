b3dde8bbab5ce422b85edfc5c7cdae00c26c2d9d
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-13 13:27:39 +0530

    CREATE 1.selection-sort.cpp

---
+++ Striver-A2Z/C++/step-2-learn-important-sorting-techniques/step-2.1-soring-I/1.selection-sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+  for (int i = 0; i <= n - 2; i++) {
+    int mini = i;
+    for (int j = i; j <= n - 1; j++) {
+      if (arr[j] < arr[mini]) {
+        mini = j;
+      }
+    }
+    int temp = arr[mini];
+    arr[mini] = arr[i];
+    arr[i] = temp;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  selectionSort(arr, n);
+
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+
+  return 0;
+}
