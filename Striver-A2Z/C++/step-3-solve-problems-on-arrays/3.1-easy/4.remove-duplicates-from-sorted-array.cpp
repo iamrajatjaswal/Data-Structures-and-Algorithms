4a6f248ef63719ecfd82e2bb4a0c6f8204b8e6e5
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-14 10:35:22 +0530

    CREATE 4.remove-duplicates-from-sorted-array.cpp

---
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.1-easy/4.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+Problem Statement:
+You are given a sorted integer array 'arr' of size 'n'. You need to remove duplicates from the array such that each element appears only once. Return the length of this new array.
+
+Note:
+Do not allocate extra space for another array. You need to do this by modifying the given input array in-place with O(1) extra memory.
+
+*/
+
+int removeDuplicates(vector<int> &arr, int n) {
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[j] != arr[i]) {
+      arr[++i] = arr[j];
+    }
+  }
+
+  // we need to increment the returned i because the i was at the last index of the unique element but the number of elements is always index of the array + 1.
+  return i + 1;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  int output = removeDuplicates(arr, n);
+  cout << output << endl;
+  return 0;
+}
