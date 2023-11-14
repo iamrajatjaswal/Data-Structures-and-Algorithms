51fec1d2952ee2a53d04c7037e72b1288cbf2b58
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-14 10:51:04 +0530

    UPDATE 4.remove-duplicates-from-sorted-array.cpp :: Added brute solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.1-easy/4.remove-duplicates-from-sorted-array.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.1-easy/4.remove-duplicates-from-sorted-array.cpp
@@ -2,25 +2,44 @@
 using namespace std;
 
 /*
-
 Problem Statement:
 You are given a sorted integer array 'arr' of size 'n'. You need to remove duplicates from the array such that each element appears only once. Return the length of this new array.
 
 Note:
 Do not allocate extra space for another array. You need to do this by modifying the given input array in-place with O(1) extra memory.
-
 */
 
+/*
+Brute Solution:
+Method: Using set datatype
+TC: O(N * logN) + O(N)
+SC: O(N)
+*/
+int removeDuplicatesBrute(vector<int> &arr, int n) {
+  set<int> uniqueSet;
+  // For above operation TC is O(N * logN);
+  for (int i = 0; i < n; i++) {
+    uniqueSet.insert(arr[i]);
+  }
+
+  // TC is O(N) for below loop
+  int index = 0;
+  for (auto it : uniqueSet) {
+    arr[index] = it;
+    index++;
+  }
+
+  // returning the number of the elements
+  return index;
+}
 
 /*
-
 Optimal Solution
 Method: Two Pointers
 Time Complexity: O(N)
 Space Complexity: O(1)
-
 */
-int removeDuplicates(vector<int> &arr, int n) {
+int removeDuplicatesOptimal(vector<int> &arr, int n) {
   int i = 0;
   for (int j = 1; j < n; j++) {
     if (arr[j] != arr[i]) {
@@ -41,7 +60,8 @@ int main() {
     cin >> arr[i];
   }
 
-  int output = removeDuplicates(arr, n);
+  int output = removeDuplicatesBrute(arr, n);
+  // int output = removeDuplicatesOptimal(arr, n);
   cout << output << endl;
   return 0;
 }
