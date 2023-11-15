b49288ff513a7348d996678a9295d7f00630c6c0
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-15 12:04:05 +0530

    UPDATE 11.union-of-two-sorted-arrays.cpp : Added optimal solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.1-easy/11.union-of-two-sorted-arrays.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.1-easy/11.union-of-two-sorted-arrays.cpp
@@ -7,7 +7,7 @@ TC -> O(n1logn) + O(n2logn) + O(n1 + n2)
 SC -> O(n1 + n2) + O(n1 + n2)
 
 */
-vector<int> unionOfSortedArrayBrute(vector<int> a, vector<int> b) {
+vector<int> unionOfTwoSortedArraysBrute(vector<int> a, vector<int> b) {
   int n1 = a.size();
   int n2 = b.size();
   set<int> st;
@@ -28,6 +28,50 @@ vector<int> unionOfSortedArrayBrute(vector<int> a, vector<int> b) {
   return unionArr;
 }
 
+/*
+Method: Optimal
+TC -> O(n1 + n2) : Because we are visiting every element once
+SC -> O(n1 + n2) : If all the elements are same and that if for returning the answer
+
+*/
+vector<int> unionOfTwoSortedArraysOptimal(vector<int> a, vector<int> b) {
+  int n1 = a.size();
+  int n2 = b.size();
+  int i = 0;
+  int j = 0;
+  vector<int> unionArr;
+
+  while (i < n1 && j < n2) {
+    if (a[i] <= b[j]) {
+      if (unionArr.size() == 0 || unionArr.back() != a[i]) {
+        unionArr.push_back(a[i]);
+      }
+      i++;
+    } else {
+      if (unionArr.size() == 0 || unionArr.back() != b[j]) {
+        unionArr.push_back(b[j]);
+      }
+      j++;
+    }
+  }
+
+  while (i < n1) {
+    if (unionArr.size() == 0 || unionArr.back() != a[i]) {
+      unionArr.push_back(a[i]);
+    }
+    i++;
+  }
+
+  while (j < n2) {
+    if (unionArr.size() == 0 || unionArr.back() != b[j]) {
+      unionArr.push_back(b[j]);
+    }
+    j++;
+  }
+
+  return unionArr;
+}
+
 int main() {
   int n1, n2;
   cin >> n1 >> n2;
@@ -42,7 +86,8 @@ int main() {
     cin >> b[i];
   }
 
-  vector<int> output = unionOfSortedArrayBrute(a, b);
+  // vector<int> output = unionOfTwoSortedArraysBrute(a, b);
+  vector<int> output = unionOfTwoSortedArraysOptimal(a, b);
 
   for (auto it : output) {
     cout << it << endl;
