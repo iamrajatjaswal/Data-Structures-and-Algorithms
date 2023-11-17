ff4b16a6080750042b3cf90f0808f053f6896949
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2023-11-17 12:46:45 +0000

    UPDATE 13.find-the-missing-number-in-an-array.cpp : Added Better solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.1-easy/13.find-the-missing-number-in-an-array.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.1-easy/13.find-the-missing-number-in-an-array.cpp
@@ -6,6 +6,7 @@ Method: Brute
 TC -> O(N^2) : Because in a hypothetical scenario it can be possible that the loop runs till the end and the last number is the missing number
 SC -> O(1)
 */
+
 int missingNumberBrute(vector<int> arr, int N, int n) {
   // Looping till total numbers to find the missing numbers
   for (int i = 1; i <= N; i++) {
@@ -26,16 +27,43 @@ int missingNumberBrute(vector<int> arr, int N, int n) {
   return 0;
 }
 
+/*
+Method: Better
+TC -> O(N) + O(N) = O(2N) : because we are looping twice
+SC -> O(N) : because we are using the hash array
+*/
+int missingNumberBetter(vector<int> arr, int N, int n) {
+  vector<int> hash(N + 1, 0);
+
+  for (int i = 0; i < n; i++) {
+    hash[arr[i]] = 1;
+  }
+
+  for (int i = 1; i <= N; i++) {
+    if (hash[i] == 0) {
+      return i;
+    }
+  }
+
+  return 0;
+}
+
 int main() {
   int n, N;
   cin >> n >> N;
   vector<int> arr(n);
+  /*
+    Inputs:
+    8 9
+    1 2 3 4 6 7 8 9
+  */
 
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
 
-  int output = missingNumberBrute(arr, N, n);
+  // int output = missingNumberBrute(arr, N, n);
+  int output = missingNumberBetter(arr, N, n);
 
   cout << output;
 
