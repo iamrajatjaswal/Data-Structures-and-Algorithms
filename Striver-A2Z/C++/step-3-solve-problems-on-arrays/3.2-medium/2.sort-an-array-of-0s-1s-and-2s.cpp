3f6a3df3116616cad795c237ca68d2993937637b
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-21 02:08:43 +0530

    UPDATE 2.sort-an-array-of-0s-1s-and-2s.cpp : added mergeSort code for brute function myself again

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/2.sort-an-array-of-0s-1s-and-2s.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/2.sort-an-array-of-0s-1s-and-2s.cpp
@@ -5,15 +5,42 @@ void merge(vector<int> &arr, int low, int mid, int high) {
   vector<int> temp;
   // [low ... mid]
   // [mid + 1 ... high]
+
   int left = low;
   int right = mid + 1;
+
+  while (left <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push_back(arr[left]);
+      left++;
+    } else {
+      temp.push_back(arr[right]);
+      right++;
+    }
+  }
+
+  while (left <= mid) {
+    temp.push_back(arr[left]);
+    left++;
+  }
+
+  while (right <= high) {
+    temp.push_back(arr[right]);
+    right++;
+  }
+
+  for (int i = low; i <= high; i++) {
+    arr[i] = temp[i - low];
+  }
 }
 
 void mergeSort(vector<int> &arr, int low, int high) {
-  if (low == high)
+  if (low == high) {
     return;
+  }
 
   int mid = (low + high) / 2;
+
   mergeSort(arr, low, mid);
   mergeSort(arr, mid + 1, high);
 
