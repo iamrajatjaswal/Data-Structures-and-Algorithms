78e32dc3245bd3a7e603328e9a602518f1ce8e00
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-11-21 12:56:38 +0530

    UPDATE 3.majority-element.cpp :: added optimal solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/3.majority-element.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/3.majority-element.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 /*
 Method: Brute
+Approach: Nested Loop
 TC -> O(N^2)
 SC -> O(1)
 */
@@ -27,6 +28,7 @@ int majorityElementBrute(vector<int> &nums) {
 
 /*
 Method: Better
+Approach: Using map datastructure and storing the count of the element
 TC -> O(N * logN) + O(N)
 SC -> O(N) : in the worst case this will only happen if the array contains all the unique elements
 */
@@ -42,6 +44,41 @@ int majorityElementBetter(vector<int> &nums) {
     if (it.second > n / 2) {
       return it.first;
     }
+  }
+
+  return -1;
+}
+
+/*
+Method: Optimal
+Approach: Moore's Voting Algorithm
+TC -> O(N) + O(N) : the second O(N) will not be done if it is stated in the problem that there always exist a majority element or if there is probability that there might not exist the majority element then we will have to vefiry with second O(N) and the time complexity become O(2N)
+SC -> O(1)
+*/
+int majorityElementOptimal(vector<int> &nums) {
+  int n = nums.size();
+  int count = 0;
+  int el;
+
+  for (int i = 0; i < n; i++) {
+    if (count == 0) {
+      count = 1;
+      el = nums[i];
+    } else if (el == nums[i]) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  int verifyCount = 0;
+  for (int i = 0; i < n; i++) {
+    if (nums[i] == el)
+      verifyCount++;
+  }
+
+  if (verifyCount > n / 2) {
+    return el;
   }
 
   return -1;
@@ -63,7 +100,8 @@ int main() {
   }
 
   // int output = majorityElementBrute(arr);
-  int output = majorityElementBetter(arr);
+  // int output = majorityElementBetter(arr);
+  int output = majorityElementOptimal(arr);
 
   cout << output;
 
