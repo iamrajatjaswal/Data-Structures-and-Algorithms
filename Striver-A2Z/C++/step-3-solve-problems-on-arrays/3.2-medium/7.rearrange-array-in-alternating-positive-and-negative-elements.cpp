ed5e539a9c221ec84cc3e05a2f12393e12356435
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2023-11-22 15:07:56 +0000

    UPDATE 7.rearrange-array-in-alternating-positive-and-negative-elements.cpp : added variety 2 solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/7.rearrange-array-in-alternating-positive-and-negative-elements.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/7.rearrange-array-in-alternating-positive-and-negative-elements.cpp
@@ -52,13 +52,64 @@ vector<int> rearrangeArrayOptimal(vector<int> &nums) {
   return result;
 }
 
+vector<int> rearrangeArrayVariety2(vector<int> &nums) {
+  int n = nums.size();
+  vector<int> positiveElements;
+  vector<int> negativeElements;
+
+  for (int i = 0; i < n; i++) {
+    if (nums[i] < 0) {
+      negativeElements.push_back(nums[i]);
+    } else {
+      positiveElements.push_back(nums[i]);
+    }
+  }
+
+  if (positiveElements.size() > negativeElements.size()) {
+    for (int i = 0; i < negativeElements.size(); i++) {
+      nums[2 * i] = positiveElements[i];
+      nums[2 * i + 1] = negativeElements[i];
+    }
+
+    int startingIndex = 2 * negativeElements.size();
+    for (int i = negativeElements.size(); i < positiveElements.size(); i++) {
+      nums[startingIndex] = positiveElements[i];
+      startingIndex++;
+    }
+  } else {
+    for (int i = 0; i < positiveElements.size(); i++) {
+      nums[2 * i] = positiveElements[i];
+      nums[2 * i + 1] = negativeElements[i];
+    }
+
+    int startingIndex = 2 * positiveElements.size();
+    for (int i = positiveElements.size(); i < negativeElements.size(); i++) {
+      nums[startingIndex] = negativeElements[i];
+      startingIndex++;
+    };
+  }
+
+  return nums;
+}
+
 int main() {
+  // Variety I : If postive & negative elements are equal
   vector<int> arr = {3, 1, -2, -5, 2, -4};
 
   // vector<int> output = rearrangeArrayBrute(arr);
   vector<int> output = rearrangeArrayOptimal(arr);
 
   for (auto it : output) {
+    cout << it << " ";
+  }
+  cout << endl;
+
+  // Variety II : If postive & negative elements are not equal
+  vector<int> arr2 = {-1, 2, 3, 4, -3, 1, -7, -8, -11, -4, 9};
+
+  vector<int> output2 = rearrangeArrayVariety2(arr2);
+
+  for (auto it : output2) {
     cout << it << " ";
   }
 
