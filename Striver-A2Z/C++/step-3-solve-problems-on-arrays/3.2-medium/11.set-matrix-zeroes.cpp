6dd54487f711cba06a31430a8676c74cb45b0450
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2023-11-27 12:20:43 +0000

    UPDATE 11.set-matrix-zeroes.cpp : added optimal solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/11.set-matrix-zeroes.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/11.set-matrix-zeroes.cpp
@@ -118,11 +118,78 @@ vector<vector<int>> setMatrixZeroesBetter(vector<vector<int>> &matrix) {
   return matrix;
 }
 
+/*
+Method: Optimal
+TC -> O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+Reason: In this approach, we are also traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+
+SC -> O(1) as we are not using any extra space.
+*/
+vector<vector<int>> setMatrixZeroesOptimal(vector<vector<int>> &matrix) {
+  int n = matrix.size();
+  int m = matrix[0].size();
+
+  // row array :: matrix[0][i]
+  // col array :: matrix[i][0]
+  int col0 = 1;
+
+  // Original Matrix
+  // 1 1 1 1
+  // 1 0 1 1
+  // 1 1 0 1
+  // 0 1 1 1
+
+  // Step 1: Traverse through the array and mark the first row and col accordingly
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (matrix[i][j] == 0) {
+        // mark the i-th row with 0
+        matrix[i][0] = 0;
+
+        // mark the j-th col with 0
+        if (j != 0) {
+          matrix[0][j] = 0;
+        } else {
+          col0 = 0;
+        }
+      }
+    }
+  }
+
+  // Step 2: Mark with 0 from (1, 1) to (n - 1, n - 1)
+  for (int i = 1; i < n; i++) {
+    for (int j = 1; j < m; j++) {
+      if (matrix[i][j] != 0) {
+        // check for col and row
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+
+  // Step 3: Finally mark the first col and the first row
+  if (matrix[0][0] == 0) {
+    for (int j = 1; j < m; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  if (col0 == 0) {
+    for (int i = 0; i < n; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  return matrix;
+}
+
 int main() {
-  vector<vector<int>> matrix = {{1, 1, 1, 1}, {1, 0, 1, 1}, {1, 1, 0, 1}, {1, 0, 0, 1}};
+  vector<vector<int>> matrix = {{1, 1, 1, 1}, {1, 0, 1, 1}, {1, 1, 0, 1}, {0, 1, 1, 1}};
 
   // vector<vector<int>> output = setMatrixZeroesBrute(matrix);
-  vector<vector<int>> output = setMatrixZeroesBetter(matrix);
+  // vector<vector<int>> output = setMatrixZeroesBetter(matrix);
+  vector<vector<int>> output = setMatrixZeroesOptimal(matrix);
 
   cout << "The final matrix is: " << endl;
   for (auto it : output) {
