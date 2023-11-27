2f8fd318c8e147c439d60e1e58e855000440d021
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2023-11-27 15:57:59 +0000

    UPDATE 12.rotate-image.cpp : added optimal solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/12.rotate-image.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/12.rotate-image.cpp
@@ -19,11 +19,41 @@ vector<vector<int>> rotateBrute(vector<vector<int>> &matrix) {
   return rotated;
 }
 
+/*
+Method: Optimal
+TC -> O(N*N) + O(N*N).One O(N*N) is for transposing the matrix and the other is for reversing the matrix.
+SC -> O(1)
+*/
+vector<vector<int>> rotateOptimal(vector<vector<int>> &matrix) {
+  int n = matrix.size();
+  // transpose the given matrix
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      swap(matrix[i][j], matrix[j][i]);
+    }
+  }
+
+  // reverse each row of the matrix
+  for (int i = 0; i < n; i++) {
+    int left = 0;
+    int right = n - 1;
+    while (left < right) {
+      swap(matrix[i][left], matrix[i][right]);
+      left++;
+      right--;
+    }
+  }
+
+  return matrix;
+}
+
 int main() {
   vector<vector<int>> arr;
   arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-  vector<vector<int>> rotated = rotateBrute(arr);
+  // vector<vector<int>> rotated = rotateBrute(arr);
+  vector<vector<int>> rotated = rotateOptimal(arr);
+
   cout << "Rotated Image:" << endl;
   for (int i = 0; i < rotated.size(); i++) {
     for (int j = 0; j < rotated[0].size(); j++) {
