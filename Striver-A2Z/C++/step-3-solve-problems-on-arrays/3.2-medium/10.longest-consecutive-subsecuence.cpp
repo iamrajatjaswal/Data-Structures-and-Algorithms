d84209316f3781579ae6ef966a4a134c7be9c3aa
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2023-11-27 09:39:31 +0000

    UPDATE 10.longest-consecutive-subsecuence.cpp : added optimal solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/10.longest-consecutive-subsecuence.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/10.longest-consecutive-subsecuence.cpp
@@ -70,12 +70,37 @@ int longestConsecutiveSubsecuenceBetter(vector<int> &nums) {
   return longest;
 }
 
+/*
+Method: Optimal
+TC ->  O(N) + O(2*N) ~ O(3*N), where N = size of the array.
+Reason: O(N) for putting all the elements into the set data structure. After that for every starting element, we are finding the consecutive elements. Though we are using nested loops, the set will be traversed at most twice in the worst case. So, the time complexity is O(2*N) instead of O(N2).
+SC -> O(N) : as we are using the set data structure to solve this problem.
+*/
 int longestConsecutiveSubsecuenceOptimal(vector<int> &nums) {
   int n = nums.size();
   if (n == 0)
     return 0;
 
   int longest = 1;
+  unordered_set<int> st;
+
+  for (int i = 0; i < n; i++) {
+    st.insert(nums[i]);
+  }
+
+  for (auto it : st) {
+    if (st.find(it - 1) == st.end()) {
+      int count = 1;
+      int x = it;
+
+      while (st.find(x + 1) != st.end()) {
+        count++;
+        x++;
+      }
+
+      longest = max(longest, count);
+    }
+  }
 
   return longest;
 }
