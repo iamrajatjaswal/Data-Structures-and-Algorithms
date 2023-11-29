3413cf411ad4b4748b51eee45a827c1cb8ba6415
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2023-11-29 12:47:03 +0000

    UPDATE 14.count-subarrays-with-given-sum.cpp : added better solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/14.count-subarrays-with-given-sum.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/14.count-subarrays-with-given-sum.cpp
@@ -30,11 +30,39 @@ int findCountOfSubarraysWithGivenSumBrute(vector<int> &nums, int k) {
   return count;
 }
 
+/*
+Method: Better
+
+TC -> O(N2), where N = size of the array.
+Reason: We are using two nested loops here. As each of them is running for exactly N times, the time complexity will be approximately O(N2).
+
+SC -> O(1) as we are not using any extra space.
+*/
+int findCountOfSubarraysWithGivenSumBetter(vector<int> &nums, int k) {
+  int n = nums.size();
+  int count = 0;
+
+  for (int i = 0; i < n; i++) {
+    int sum = 0;
+
+    for (int j = i; j < n; j++) {
+      sum += nums[j];
+
+      if (sum == k) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
 int main() {
   vector<int> arr = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
   int k = 3;
 
-  int count = findCountOfSubarraysWithGivenSumBrute(arr, k);
+  // int count = findCountOfSubarraysWithGivenSumBrute(arr, k);
+  int count = findCountOfSubarraysWithGivenSumBetter(arr, k);
 
   cout << "The number of subarrays is: " << count << endl;
 
