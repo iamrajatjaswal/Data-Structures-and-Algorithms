6ccffe527369c4b452cc877d9d24c594c89d79a4
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-02 19:42:32 +0530

    UPDATE 14.count-subarrays-with-given-sum.cpp : added optimal solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/14.count-subarrays-with-given-sum.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/14.count-subarrays-with-given-sum.cpp
@@ -57,12 +57,71 @@ int findCountOfSubarraysWithGivenSumBetter(vector<int> &nums, int k) {
   return count;
 }
 
+int findCountOfSubarraysWithGivenSumOptimal(vector<int> &nums, int k) {
+  int n = nums.size(); // size of the given array.
+  map<int, int> mpp;
+  int preSum = 0, count = 0;
+
+  mpp[0] = 1; // Setting 0 in the map.
+
+  cout << "mpp ==> " << endl;
+  for (auto it : mpp) {
+    cout << it.first << "," << it.second << endl;
+  }
+  cout << "==========================" << endl;
+
+  for (int i = 0; i < n; i++) {
+    // add current element to prefix Sum:
+    preSum += nums[i]; // 1 + 2 + 3 -3 + 1 + 1 + 1 + 4 + 2 - 3 = 9
+
+    cout << "nums[i] ==> " << nums[i] << endl;
+    cout << "preSum ==> " << preSum << endl;
+
+    // Calculate x-k:
+    int remove = preSum - k; // 9 - 3 = 6
+
+    cout << "remove ==> " << preSum << " - " << k << " ==> " << remove << endl;
+
+    // Add the number of subarrays to be removed:
+    count += mpp[remove]; // 7
+
+    cout << "count ==> " << count << endl;
+
+    // Update the count of prefix sum
+    // in the map.
+    mpp[preSum] += 1; // 6, 3
+
+    cout << "mpp ==> " << endl;
+    for (auto it : mpp) {
+      cout << it.first << "," << it.second << endl;
+    }
+    cout << "==========================" << endl;
+    /*
+    Map Data Structure key value pairs
+    -2,0
+    0,1
+    1,1
+    2,0
+    3,2
+    4,1
+    5,1
+    6,2
+    7,0
+    9,1
+    10,1
+    12,1
+    */
+  }
+  return count;
+}
+
 int main() {
   vector<int> arr = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
   int k = 3;
 
   // int count = findCountOfSubarraysWithGivenSumBrute(arr, k);
-  int count = findCountOfSubarraysWithGivenSumBetter(arr, k);
+  // int count = findCountOfSubarraysWithGivenSumBetter(arr, k);
+  int count = findCountOfSubarraysWithGivenSumOptimal(arr, k);
 
   cout << "The number of subarrays is: " << count << endl;
 
