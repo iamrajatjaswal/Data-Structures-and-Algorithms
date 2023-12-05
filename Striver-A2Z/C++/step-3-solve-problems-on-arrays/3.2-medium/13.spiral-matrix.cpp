ed172a99e97d128ee79adda09ff1bf6caa5a4140
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-05 22:29:22 +0530

    CREATE 05. 1688. Count of Matches in Tournament.js

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/13.spiral-matrix.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.2-medium/13.spiral-matrix.cpp
@@ -48,7 +48,7 @@ vector<int> printSpiral(vector<vector<int>> &matrix) {
   }
 
   return ans;
-}
+} 
 
 int main() {
   vector<vector<int>> mat = {{1, 2, 3, 4, 5, 6}, {20, 21, 22, 23, 24, 7}, {19, 32, 33, 34, 25, 8}, {18, 31, 36, 35, 26, 9}, {17, 30, 29, 28, 27, 10}, {16, 15, 14, 13, 12, 11}};
