8b2b8d667aec3e785d78c44fcddfac55c3b6f741
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-20 11:46:01 +0530

    CREATE 19. 2706. Buy Two Chocolates.js

---
+++ LeetCode/2023 - 12 - December - 31 Day LeetCode Challenge/Week 3 December 15th - December 21st/19. 2706. Buy Two Chocolates.js
@@ -0,0 +1,70 @@
+/*
+
+Title: 2706. Buy Two Chocolates
+URL: https://leetcode.com/problems/buy-two-chocolates/
+Difficulty: Easy
+Topics: Array, Sorting
+
+
+**Problem**
+
+You are given an integer array `prices` representing the prices of various chocolates in a store. You are also given a single integer `money`, which represents your initial amount of money.
+
+You must buy **exactly** two chocolates in such a way that you still have some **non-negative** leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+
+Return *the amount of money you will have leftover after buying the two chocolates*. If there is no way for you to buy two chocolates without ending up in debt, return `money`. Note that the leftover must be non-negative.
+
+**Example 1:**
+
+```
+Input: prices = [1,2,2], money = 3
+Output: 0
+Explanation: Purchase the chocolates priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money afterwards. Thus, we return 0.
+
+```
+
+**Example 2:**
+
+```
+Input: prices = [3,2,3], money = 3
+Output: 3
+Explanation: You cannot buy 2 chocolates without going in debt, so we return 3.
+
+```
+
+**Constraints:**
+
+- `2 <= prices.length <= 50`
+- `1 <= prices[i] <= 100`
+- `1 <= money <= 100`
+
+
+**Solution**
+*/
+
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+var buyChoco = function(arr, key) {
+  let ans = Infinity;
+      let fin;
+
+      for (let i = 0; i < arr.length; ++i) {
+          for (let j = 0; j < arr.length; ++j) {
+              if (i !== j) {
+                  const sum = arr[i] + arr[j];
+                  ans = Math.min(ans, sum);
+              }
+          }
+      }
+
+      fin = key - ans;
+      if(fin>=0){
+      return fin;
+      }
+      else{
+          return key;
+      }
+};
