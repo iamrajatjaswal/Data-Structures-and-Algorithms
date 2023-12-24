cdf470a20cf90418f6db9855f73cdd3506396c9d
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-24 22:03:28 +0530

    UPDATE 07.merge-overlapping-intervals.cpp : added optimal solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/07.merge-overlapping-intervals.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/07.merge-overlapping-intervals.cpp
@@ -37,11 +37,44 @@ vector<vector<int>> mergeOverlappingIntervalsBrute(vector<vector<int>> &interval
   return ans;
 }
 
+/*
+Method: Optimal
+
+TC -> O(N*logN) + O(N), where N = the size of the given array.
+Reason: Sorting the given array takes  O(N*logN) time complexity. Now, after that, we are just using a single loop that runs for N times. So, the time complexity will be O(N).
+
+SC -> O(N), as we are using an answer list to store the merged intervals. Except for the answer array, we are not using any extra space.
+*/
+vector<vector<int>> mergeOverlappingIntervalsOptimal(vector<vector<int>> &intervals) {
+  int n = intervals.size(); // size of the arary
+
+  // sort the given intervals
+  sort(intervals.begin(), intervals.end());
+
+  vector<vector<int>> ans;
+
+  for (int i = 0; i < n; i++) {
+    // if the current interval does not lie in the last interval
+    if (ans.empty() || intervals[i][0] > ans.back()[1]) {
+      ans.push_back(intervals[i]);
+    } else {
+      // if the current interval lies in the last interval
+      ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+    }
+  }
+
+  return ans;
+}
+
 int main() {
   vector<vector<int>> arr = {{1, 3}, {2, 6}, {8, 9}, {9, 11}, {8, 10}, {2, 4}, {15, 18}, {16, 17}, {15, 18}};
   // expected output: [[1,6], [8, 11], [15, 18]]
 
-  vector<vector<int>> output = mergeOverlappingIntervalsBrute(arr);
+  // vector<vector<int>> output = mergeOverlappingIntervalsBrute(arr);
+  /*
+
+  */
+  vector<vector<int>> output = mergeOverlappingIntervalsOptimal(arr);
 
   cout << "The merged intervals are: " << endl;
   for (auto it : output) {
