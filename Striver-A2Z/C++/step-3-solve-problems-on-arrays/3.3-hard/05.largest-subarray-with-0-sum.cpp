5e44d646eb300b7fb7ecc259bdb3312a8b6f590e
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-24 14:16:46 +0530

    CREATE 05.largest-subarray-with-0-sum.cpp : added brute solution

---
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/05.largest-subarray-with-0-sum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Method: Brute
+TC -> O(N^3)
+SC -> O(1)
+*/
+int largestSubarrayWith0SumBrute(vector<int> &nums) {
+  int n = nums.size();
+  int maxLen = 0;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      int sum = 0;
+      for (int k = i; k <= j; k++) {
+        sum += nums[k];
+      }
+
+      if (sum == 0) {
+        maxLen = max(maxLen, j - i + 1);
+      }
+    }
+  }
+
+  return maxLen;
+}
+
+int main() {
+  vector<int> arr = {9, -3, 3, -1, 6, -5};
+
+  int output = largestSubarrayWith0SumBrute(arr);
+  cout << "The lenght of the longest subarray is: " << output << endl;
+
+  return 0;
+}
