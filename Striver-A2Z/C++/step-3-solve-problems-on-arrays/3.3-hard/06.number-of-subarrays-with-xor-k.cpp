b2ee986c7f3cdb5468f07d4636a2b544c963e04a
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-24 15:58:51 +0530

    UPDATE 06.number-of-subarrays-with-xor-k.cpp : added better solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/06.number-of-subarrays-with-xor-k.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/06.number-of-subarrays-with-xor-k.cpp
@@ -30,10 +30,36 @@ int numberOfSubarraysWithXorKBrute(vector<int> &nums, int k) {
   return cnt;
 }
 
+/*
+Method: Better
+TC -> O(N^2)
+SC -> O(1)
+*/
+int numberOfSubarraysWithXorKBetter(vector<int> &nums, int k) {
+  int n = nums.size(); // size of the given array
+  int cnt = 0;
+
+  // step 1: generating subarrays
+  for (int i = 0; i < n; i++) {
+    int xorr = 0;
+    for (int j = i; j < n; j++) {
+      // step 2: calculate XOR of all elements
+      xorr = xorr ^ nums[j];
+      // step 3: check XOR and count
+      if (xorr == k) {
+        cnt++;
+      }
+    }
+  }
+
+  return cnt;
+}
+
 int main() {
   vector<int> arr = {4, 2, 2, 6, 4};
   int k = 6;
-  int output = numberOfSubarraysWithXorKBrute(arr, k);
+  // int output = numberOfSubarraysWithXorKBrute(arr, k);
+  int output = numberOfSubarraysWithXorKBetter(arr, k);
 
   cout << "The number of subarrays with XOR as " << k << " is: " << output << endl;
 
