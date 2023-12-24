0322ed8623830d49507a12db5b1acd1159ea852f
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-24 14:23:37 +0530

    UPDATE 05.largest-subarray-with-0-sum.cpp : updated the solution and fixed the issue

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/05.largest-subarray-with-0-sum.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/05.largest-subarray-with-0-sum.cpp
@@ -11,7 +11,7 @@ int largestSubarrayWith0SumBrute(vector<int> &nums) {
   int maxLen = 0;
 
   for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; j++) {
+    for (int j = i; j < n; j++) {
       int sum = 0;
       for (int k = i; k <= j; k++) {
         sum += nums[k];
@@ -30,7 +30,7 @@ int main() {
   vector<int> arr = {9, -3, 3, -1, 6, -5};
 
   int output = largestSubarrayWith0SumBrute(arr);
-  cout << "The lenght of the longest subarray is: " << output << endl;
+  cout << "The length of the longest subarray with sum 0 is: " << output << endl;
 
   return 0;
 }
