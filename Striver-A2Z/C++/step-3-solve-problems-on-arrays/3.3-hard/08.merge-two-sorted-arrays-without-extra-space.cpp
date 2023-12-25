26de9eee410ab7b1c01e0ab4fb235357a2768459
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-25 11:15:35 +0530

    UPDATE 08.merge-two-sorted-arrays-without-extra-space.cpp : added optimal - 1 solution

--- Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/08.merge-two-sorted-arrays-without-extra-space.cpp
+++ Striver-A2Z/C++/step-3-solve-problems-on-arrays/3.3-hard/08.merge-two-sorted-arrays-without-extra-space.cpp
@@ -52,12 +52,43 @@ void mergeBrute(long long arr1[], long long arr2[], int n, int m) {
   }
 }
 
+/*
+Method: Optimal 1
+
+TC -> O(min(n, m)) + O(n*logn) + O(m*logm), where n and m are the sizes of the given arrays.
+Reason: O(min(n, m)) is for swapping the array elements. And O(n*logn) and O(m*logm) are for sorting the two arrays.
+
+SC -> O(1) as we are not using any extra space.
+*/
+void mergeOptimal1(long long arr1[], long long arr2[], int n, int m) {
+
+  // Declare 2 pointers:
+  int left = n - 1;
+  int right = 0;
+
+  // Swap the elements until arr1[left] is
+  //  smaller than arr2[right]:
+  while (left >= 0 && right < m) {
+    if (arr1[left] > arr2[right]) {
+      swap(arr1[left], arr2[right]);
+      left--, right++;
+    } else {
+      break;
+    }
+  }
+
+  // Sort arr1[] and arr2[] individually:
+  sort(arr1, arr1 + n);
+  sort(arr2, arr2 + m);
+}
+
 int main() {
   long long arr1[] = {1, 4, 8, 10};
   long long arr2[] = {2, 3, 9};
   int n = 4, m = 3;
 
-  mergeBrute(arr1, arr2, n, m);
+  // mergeBrute(arr1, arr2, n, m);
+  mergeOptimal1(arr1, arr2, n, m);
 
   cout << "The merged arrays are: "
        << "\n";
