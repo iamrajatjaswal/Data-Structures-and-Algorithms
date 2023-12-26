60416a85245d108f056fd91449243b7d5c3a225f
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-26 09:56:57 +0530

    UPDATE 26. 1155. Number of Dice Rolls With Target Sum.js : added solution

--- LeetCode/2023 - 12 - December - 31 Day LeetCode Challenge/Week 4 December 22nd - December 28th/26. 1155. Number of Dice Rolls With Target Sum.js
+++ LeetCode/2023 - 12 - December - 31 Day LeetCode Challenge/Week 4 December 22nd - December 28th/26. 1155. Number of Dice Rolls With Target Sum.js
@@ -50,4 +50,29 @@ Explanation: The answer must be returned modulo 109 + 7.
 **Solution**
 */
 
+const mod = Math.pow(10, 9) + 7;
 
+const recursion = function(dp, n, k, target) {
+    if (target === 0 && n === 0) return 1;
+    if (n === 0 || target <= 0) return 0;
+
+    if (dp[n][target] !== -1) return dp[n][target] % mod;
+
+    let ways = 0;
+    for (let i = 1; i <= k; i++) {
+        ways = (ways + recursion(dp, n - 1, k, target - i)) % mod;
+    }
+    dp[n][target] = ways % mod;
+    return dp[n][target];
+}
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} target
+ * @return {number}
+ */
+var numRollsToTarget = function(n, k, target) {
+    const dp = Array.from({ length: n + 1 }, () => Array(target + 1).fill(-1));
+    return recursion(dp, n, k, target);
+};
