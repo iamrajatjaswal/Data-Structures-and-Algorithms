8ee5bf08eeb83fe6c8c5845a1e02fd3929826eb5
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-30 12:12:19 +0530

    UPDATE 0001. 3. Longest Substring Without Repeating Characters.js : updated commenting of the brute solutoin tc and sc

--- LeetCode/String/0001. 3. Longest Substring Without Repeating Characters.js
+++ LeetCode/String/0001. 3. Longest Substring Without Repeating Characters.js
@@ -67,11 +67,19 @@ var isRepeating = function (substring, char) {
   Method: Brute
   TC -> O(N^3)
   
-  The time complexity of the provided solution is O(n^3), where "n" is the length of the input string. This is because there are two nested loops, and for each character in the outer loop, there is a nested loop that potentially iterates over all characters in the remaining part of the string.
+  The time complexity of the provided solution is O(n^3), where "n" is the length of the input 
+  string. This is because there are two nested loops, and for each character in the outer loop, 
+  there is a nested loop that potentially iterates over all characters in the remaining part of 
+  the string.
 
-  SC ->  O(min(n, m)), where "n" is the length of the input string, and "m" is the size of the character set (assuming constant size, such as ASCII characters).
+  SC ->  O(min(n, m)), where "n" is the length of the input string, and "m" is the size of the 
+  character set (assuming constant size, such as ASCII characters).
 
-  The space complexity is determined by the storage of the currentSubstring variable. In the worst case, the substring could be as long as the entire input string, so the space complexity is O(n). However, since the substring only contains unique characters, its size is limited by the character set size ("m"). Therefore, in scenarios where the character set size is small and constant, the space complexity is O(min(n, m)).
+  The space complexity is determined by the storage of the currentSubstring variable. In the worst 
+  case, the substring could be as long as the entire input string, so the space complexity is O
+  (n). However, since the substring only contains unique characters, its size is limited by the 
+  character set size ("m"). Therefore, in scenarios where the character set size is small and 
+  constant, the space complexity is O(min(n, m)).
 */
 
 /**
