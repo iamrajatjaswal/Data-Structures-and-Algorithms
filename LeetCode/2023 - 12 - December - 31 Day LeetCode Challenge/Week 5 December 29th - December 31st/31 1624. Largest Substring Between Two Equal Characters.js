4b9587d8ff5685f0a9b10e964356848b3057007c
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-31 11:21:17 +0530

    CREATE Week 5 December 29th - December 31st -> 31 1624. Largest Substring Between Two Equal Characters.js

---
+++ LeetCode/2023 - 12 - December - 31 Day LeetCode Challenge/Week 5 December 29th - December 31st/31 1624. Largest Substring Between Two Equal Characters.js
@@ -0,0 +1,61 @@
+/*
+
+Title: 1624. Largest Substring Between Two Equal Characters
+URL: https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+Difficulty: Easy
+Topics: Hash Table, String, Counting
+
+
+**Problem**
+
+Given a string `s`, return *the length of the longest substring between two equal characters, excluding the two characters.* If there is no such substring return `-1`.
+
+A **substring** is a contiguous sequence of characters within a string.
+
+**Example 1:**
+
+```
+Input: s = "aa"
+Output: 0
+Explanation: The optimal substring here is an empty substring between the two'a's.
+```
+
+**Example 2:**
+
+```
+Input: s = "abca"
+Output: 2
+Explanation: The optimal substring here is "bc".
+
+```
+
+**Example 3:**
+
+```
+Input: s = "cbzxy"
+Output: -1
+Explanation: There are no characters that appear twice in s.
+
+```
+
+**Constraints:**
+
+- `1 <= s.length <= 300`
+- `s` contains only lowercase English letters.
+
+
+**Solution**
+*/
+
+var maxLengthBetweenEqualCharacters = function (s) {
+  let current = s.length - 1;
+  for (let i = s.length - 1; i > 0; i--) {
+    for (let j = 0; j + i < s.length; j++) {
+      if (s[j] === s[j + i]) {
+        return i - 1;
+      }
+    }
+    current--;
+  }
+  return -1;
+};
