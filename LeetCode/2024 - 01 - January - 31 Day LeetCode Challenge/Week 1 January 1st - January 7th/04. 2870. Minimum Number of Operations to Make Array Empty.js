179fcd37f449a276c0afab56ff3d51b70756b1ad
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-01-04 15:09:31 +0530

    UPDATE 04. 2870. Minimum Number of Operations to Make Array Empty.js : Added one more solution of TC -> O(2N) & SC -> O(N)

--- LeetCode/2024 - 01 - January - 31 Day LeetCode Challenge/Week 1 January 1st - January 7th/04. 2870. Minimum Number of Operations to Make Array Empty.js
+++ LeetCode/2024 - 01 - January - 31 Day LeetCode Challenge/Week 1 January 1st - January 7th/04. 2870. Minimum Number of Operations to Make Array Empty.js
@@ -77,3 +77,59 @@ var minOperations = function (nums) {
   }
   return res;
 };
+
+/*
+  Method: Hash Table
+  TC -> O(2N)
+  SC -> O(N)
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function (nums) {
+  const map = {};
+  let operations = 0;
+
+  for (let num of nums) {
+    map[num] = (map[num] || 0) + 1;
+  }
+
+  for (let i in map) {
+    let numCount = map[i];
+    if (numCount === 1) return -1;
+
+    /*
+    We are dividing by 3 first because making triplets of that number than pairs will result
+    in less number of operations
+    */
+    let rem = numCount % 3;
+
+    if (rem === 0) {
+      /*
+      3 completely divides the count that means the total number of operations will be
+      equal to the number of triplets of that particular number which will be the result
+      after dividing by 3
+      */
+      operations += Math.floor(numCount / 3);
+    } else {
+      //   } else if (rem === 1 || rem === 2) {
+      /*
+      what it means is that there will either be rem = 2 or rem = 1 
+      if rem == 1
+          then there will be pairs of three but still one number unique number of that
+          value will be left and we can't make any more pairs or triplets then that means
+          we can go for 4 and then two paris will be made such that we have divided 3
+          and got remainder 1 but we will exclude last divide from the divisions and we will
+          get a number 4 which will have 2 pairs of that number and so indirectly we can add 
+          one more operation
+      if rem == 2
+          then total number of operations for that particular number will be the floor of 
+          divisions of that number by 3 and + 1 more operation for a pair of 2 numbers
+      */
+      operations += Math.floor(numCount / 3) + 1;
+    }
+  }
+
+  return operations;
+};
