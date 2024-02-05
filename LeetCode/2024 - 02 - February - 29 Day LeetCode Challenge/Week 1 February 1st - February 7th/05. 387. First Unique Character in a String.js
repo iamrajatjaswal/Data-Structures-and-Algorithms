68a3b53fca1af7ef3cb9fa9f5c6c4d877dfa1901
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-05 14:00:29 +0530

    CREATE 05. 387. First Unique Character in a String.js

---
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 1 February 1st - February 7th/05. 387. First Unique Character in a String.js
@@ -0,0 +1,53 @@
+/*
+
+Title: 387. First Unique Character in a String
+URL: https://leetcode.com/problems/first-unique-character-in-a-string/
+Difficulty: Easy
+Topics: Hash Table, String, Queue, Counting
+
+
+**Problem**
+
+
+Given a string `s`, *find the first non-repeating character in it and return its index*. If it does not exist, return `-1`.
+
+**Example 1:**
+
+```
+Input: s = "leetcode"
+Output: 0
+
+```
+
+**Example 2:**
+
+```
+Input: s = "loveleetcode"
+Output: 2
+
+```
+
+**Example 3:**
+
+```
+Input: s = "aabb"
+Output: -1
+
+```
+
+**Constraints:**
+
+- `1 <= s.length <= 10^5`
+- `s` consists of only lowercase English letters.
+
+
+
+**Solution**
+*/
+
+/*
+  Approach
+  TC -> O(s+t)
+  SC -> O(s+t)
+*/
+
