d684fb9e93256666ec9b40dac6449376ebbe9713
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-07 13:48:28 +0530

    UPDATE 07. 451. Sort Characters By Frequency.js : Added Approach 2

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 1 February 1st - February 7th/07. 451. Sort Characters By Frequency.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 1 February 1st - February 7th/07. 451. Sort Characters By Frequency.js
@@ -84,3 +84,22 @@ var frequencySort = function (s) {
 
   return result;
 };
+
+/*
+  Approach 2
+*/
+
+var frequencySort = function (s) {
+  const mp = new Map();
+  for (const char of s) {
+    mp.set(char, (mp.get(char) || 0) + 1);
+  }
+
+  const r = new Map([...mp.entries()].sort((a, b) => b[1] - a[1]));
+  let ss = "";
+  for (const [char, freq] of r.entries()) {
+    ss += char.repeat(freq);
+  }
+
+  return ss;
+};
