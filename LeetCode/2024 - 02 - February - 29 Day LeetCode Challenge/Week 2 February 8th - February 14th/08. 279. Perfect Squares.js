36d370a8fc9846431bdd988c9aee323d2215580f
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-02-08 17:55:33 +0000

    CREATE 08. 279. Perfect Squares.js : added problem statement

---
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/08. 279. Perfect Squares.js
@@ -0,0 +1,44 @@
+/*
+
+Title: 279. Perfect Squares
+URL: https://leetcode.com/problems/sort-characters-by-frequency/
+Difficulty: Medium
+Topics: Hash Table, String, Sorting, Heap (Priority Queue), Bucket Sort, Counting
+
+
+**Problem**
+
+
+Given an integer `n`, return *the least number of perfect square numbers that sum to* `n`.
+
+A **perfect square** is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, `1`, `4`, `9`, and `16` are perfect squares while `3` and `11` are not.
+
+**Example 1:**
+
+```
+Input: n = 12
+Output: 3
+Explanation: 12 = 4 + 4 + 4.
+
+```
+
+**Example 2:**
+
+```
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+
+```
+
+**Constraints:**
+
+- `1 <= n <= 10^4`
+
+
+**Solution**
+
+*/
+
+
+
