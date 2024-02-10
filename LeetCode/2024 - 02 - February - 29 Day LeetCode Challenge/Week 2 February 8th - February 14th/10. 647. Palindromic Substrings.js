8464723d34dc8cd5b64e506b0d81f62d6b6f47e6
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-10 20:07:43 +0530

    UPDATE 10. 647. Palindromic Substrings.js : Added solution with appraoch 1

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/10. 647. Palindromic Substrings.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/10. 647. Palindromic Substrings.js
@@ -42,3 +42,35 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 **Solution**
 
 */
+
+/*
+  Approach 1
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function (s) {
+  let n = s.length;
+  let ans = 0;
+
+  if (n <= 0) return 0;
+
+  let dp = new Array(n).fill(false).map(() => new Array(n).fill(false));
+
+  for (let i = 0; i < n; ++i, ++ans) dp[i][i] = true;
+
+  for (let i = 0; i < n - 1; ++i) {
+    dp[i][i + 1] = s[i] === s[i + 1];
+    ans += dp[i][i + 1] ? 1 : 0;
+  }
+
+  for (let length = 3; length <= n; ++length)
+    for (let i = 0, j = i + length - 1; j < n; ++i, ++j) {
+      dp[i][j] = dp[i + 1][j - 1] && s[i] === s[j];
+      ans += dp[i][j] ? 1 : 0;
+    }
+
+  return ans;
+};
