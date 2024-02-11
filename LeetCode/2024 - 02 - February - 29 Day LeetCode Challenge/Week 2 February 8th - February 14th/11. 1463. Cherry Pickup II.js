acbb5be9714bbe07202e2bfbd487cf601dc635b8
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-11 11:06:06 +0530

    UPDATE 11. 1463. Cherry Pickup II.js : Added solution with approach 1

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/11. 1463. Cherry Pickup II.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/11. 1463. Cherry Pickup II.js
@@ -64,3 +64,49 @@ Total of cherries: 17 + 11 = 28.
 **Solution**
 
 */
+
+/*
+  Approach 1
+*/
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var cherryPickup = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+  const dp = Array.from({ length: m }, () =>
+    Array.from({ length: n }, () => Array(n).fill(-1))
+  );
+  dp[0][0][n - 1] = grid[0][0] + grid[0][n - 1];
+
+  let ans = 0;
+  for (let i = 1; i < m; ++i) {
+    for (let j = 0; j < n; ++j) {
+      // robotA
+      for (let k = j + 1; k < n; ++k) {
+        // robotB
+        for (let x = -1; x <= 1; ++x) {
+          // x and y all possible combinations
+          for (let y = -1; y <= 1; ++y) {
+            const nj = j + x;
+            const nk = k + y;
+            if (nj >= 0 && nj < n && nk >= 0 && nk < n) {
+              const prev = dp[i - 1][nj][nk];
+              if (prev !== -1) {
+                dp[i][j][k] = Math.max(
+                  dp[i][j][k],
+                  prev + grid[i][j] + (j !== k ? grid[i][k] : 0)
+                );
+              }
+            }
+          }
+        }
+        if (ans < dp[i][j][k]) ans = dp[i][j][k];
+      }
+    }
+  }
+
+  return ans;
+};
