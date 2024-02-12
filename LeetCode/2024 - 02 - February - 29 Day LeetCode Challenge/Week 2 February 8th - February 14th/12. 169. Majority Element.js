c35648120296fa8bbfb54798821074562b8ac6bf
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-12 10:39:10 +0530

    UPDATE 12. 169. Majority Element.js : added solution for the approach 1

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/12. 169. Majority Element.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/12. 169. Majority Element.js
@@ -42,3 +42,31 @@ Could you solve the problem in linear time and in `O(1)` space?
 **Solution**
 
 */
+
+/*
+  Approach 1    
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let map = new Map();
+  let n = nums.length / 2;
+
+  for (let num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  let ans = 0;
+
+  for (let [key, value] of map.entries()) {
+    if (value > n) {
+      ans = key;
+      break;
+    }
+  }
+
+  return ans;
+};
