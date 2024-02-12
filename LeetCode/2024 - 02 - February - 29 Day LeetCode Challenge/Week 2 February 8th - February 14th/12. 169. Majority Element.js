4c745af0cf4dc1d7f0c28f83ed2c7efa464a7cb1
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-12 10:40:25 +0530

    UPDATE 12. 169. Majority Element.js : added solution for the approach 2

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/12. 169. Majority Element.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/12. 169. Majority Element.js
@@ -78,3 +78,29 @@ var majorityElement = function (nums) {
 
   return ans;
 };
+
+/*
+  Approach 2
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let count = 0;
+  let element = 0;
+
+  for (let num of nums) {
+    if (count === 0) {
+      element = num;
+      count = 1;
+    } else if (element === num) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return element;
+};
