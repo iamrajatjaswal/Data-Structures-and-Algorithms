ef066ae88149dc4cbf0bd2fd85c2f9c380df4983
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-13 17:04:53 +0530

    UPDATE 13. 2108. Find First Palindromic String in the Array.js : added solution with approach 2

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/13. 2108. Find First Palindromic String in the Array.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/13. 2108. Find First Palindromic String in the Array.js
@@ -97,3 +97,20 @@ var firstPalindrome = function (words) {
   }
   return "";
 };
+
+/*
+  Approach 2
+*/
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function (words) {
+  for (const word of words) {
+    if (word === word.split("").reverse().join("")) {
+      return word;
+    }
+  }
+  return "";
+};
