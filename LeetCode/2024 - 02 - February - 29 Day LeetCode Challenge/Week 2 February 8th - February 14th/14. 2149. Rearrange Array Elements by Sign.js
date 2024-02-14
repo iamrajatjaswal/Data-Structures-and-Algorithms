d9de97f555f7b9704e23bccc6ad18c5e2f4e478a
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-14 17:47:56 +0530

    UPDATE 14. 2149. Rearrange Array Elements by Sign.js : added solution for approach 2

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/14. 2149. Rearrange Array Elements by Sign.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/14. 2149. Rearrange Array Elements by Sign.js
@@ -88,4 +88,28 @@ var rearrangeArray = function (nums) {
   return ans;
 };
 
+/*
+  Approach 2
+*/
 
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function (nums) {
+  let ans = new Array(nums.length).fill(0);
+  let pos = 0,
+    neg = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      ans[pos] = nums[i];
+      pos += 2;
+    } else {
+      ans[neg] = nums[i];
+      neg += 2;
+    }
+  }
+
+  return ans;
+};
