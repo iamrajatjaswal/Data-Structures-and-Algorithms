f9d681e608a6eec6ffcd2715b62f6b2abb404c8e
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-15 16:21:17 +0530

    UPDATE 15. 2971. Find Polygon With the Largest Perimeter.js : Added solution with Approach 2

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/15. 2971. Find Polygon With the Largest Perimeter.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 2 February 8th - February 14th/15. 2971. Find Polygon With the Largest Perimeter.js
@@ -86,3 +86,25 @@ var largestPerimeter = function (nums) {
   }
   return -1;
 };
+
+/*
+  Approach 2 ( With Forward Iteration )
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function (nums) {
+  nums.sort((a, b) => a - b);
+  let sum = 0;
+  let ans = -1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < sum) {
+      ans = nums[i] + sum;
+    }
+    sum += nums[i];
+  }
+
+  return ans;
+};
