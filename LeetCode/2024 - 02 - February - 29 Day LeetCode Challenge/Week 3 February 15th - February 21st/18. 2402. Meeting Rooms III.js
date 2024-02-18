4f49ae95b9b78e18aafbf8061920f16c4d76c9f8
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-18 11:29:35 +0530

    UPDATE 18. 2402. Meeting Rooms III.js : Added problem solution with approach 1

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 3 February 15th - February 21st/18. 2402. Meeting Rooms III.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 3 February 15th - February 21st/18. 2402. Meeting Rooms III.js
@@ -69,3 +69,50 @@ Room 0 held 1 meeting while rooms 1 and 2 each held 2 meetings, so we return 1.
 **Solution**
 
 */
+
+/*
+  Approach 1
+*/
+/**
+ * @param {number} n
+ * @param {number[][]} meetings
+ * @return {number}
+ */
+var mostBooked = function (n, meetings) {
+  const ans = new Array(n).fill(0);
+  const times = new Array(n).fill(0);
+  meetings.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 0; i < meetings.length; i++) {
+    const [start, end] = meetings[i];
+    let flag = false;
+    let minind = -1;
+    let val = Number.MAX_SAFE_INTEGER;
+    for (let j = 0; j < n; j++) {
+      if (times[j] < val) {
+        val = times[j];
+        minind = j;
+      }
+      if (times[j] <= start) {
+        flag = true;
+        ans[j]++;
+        times[j] = end;
+        break;
+      }
+    }
+    if (!flag) {
+      ans[minind]++;
+      times[minind] += end - start;
+    }
+  }
+
+  let maxi = -1;
+  let id = -1;
+  for (let i = 0; i < n; i++) {
+    if (ans[i] > maxi) {
+      maxi = ans[i];
+      id = i;
+    }
+  }
+  return id;
+};
