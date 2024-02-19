b1bd06e84225f9bb21b4494a60d4223a814bb6ef
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-19 13:46:22 +0530

    CREATE 19. 231. Power of Two.js

---
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 3 February 15th - February 21st/19. 231. Power of Two.js
@@ -0,0 +1,54 @@
+/*
+
+Title: 231. Power of Two
+URL: https://leetcode.com/problems/power-of-two/
+Difficulty: Easy
+Topics: Math, Bit Manipulation, Recursion
+
+
+**Problem**
+
+
+Given an integer `n`, return *`true` if it is a power of two. Otherwise, return `false`*.
+
+An integer `n` is a power of two, if there exists an integer `x` such that `n == 2x`.
+
+**Example 1:**
+
+```
+Input: n = 1
+Output: true
+Explanation:20 = 1
+
+```
+
+**Example 2:**
+
+```
+Input: n = 16
+Output: true
+Explanation:24 = 16
+
+```
+
+**Example 3:**
+
+```
+Input: n = 3
+Output: false
+
+```
+
+**Constraints:**
+
+- `2^31 <= n <= 2^31 - 1`
+
+**Follow up:**
+
+Could you solve it without loops/recursion?
+
+
+
+**Solution**
+
+*/
