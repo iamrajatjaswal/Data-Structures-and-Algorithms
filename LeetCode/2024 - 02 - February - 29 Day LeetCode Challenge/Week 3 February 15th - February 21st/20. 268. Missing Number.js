1c80df94f1c8d7031e2da5826feffca33530a43a
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-02-20 17:08:37 +0000

    UPDATE 20. 268. Missing Number.js : Added solution with approach 3

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 3 February 15th - February 21st/20. 268. Missing Number.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 3 February 15th - February 21st/20. 268. Missing Number.js
@@ -90,3 +90,17 @@ var missingNumber = function(nums) {
   }
   return ans;
 };
+
+/*
+  Approach 3
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  const n = nums.length;
+  const Tsum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((acc, num) => acc + num, 0);
+  return Tsum - actualSum;
+};
