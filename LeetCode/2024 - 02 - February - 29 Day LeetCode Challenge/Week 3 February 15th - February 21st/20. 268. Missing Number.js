79031ccd178f1dba1eed2c5a5783ce2748a40233
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-02-20 17:06:56 +0000

    UPDATE 20. 268. Missing Number.js : Added solution with approach 1

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 3 February 15th - February 21st/20. 268. Missing Number.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 3 February 15th - February 21st/20. 268. Missing Number.js
@@ -52,3 +52,22 @@ Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0
 **Solution**
 
 */
+
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  let n = nums.length;
+  let v = new Array(n+1).fill(-1);
+  for(let i = 0; i < nums.length; i++) {
+      v[nums[i]] = nums[i];
+  }
+  for(let i = 0; i < v.length; i++) {
+      if(v[i] == -1) return i;
+  }
+  return 0;
+};
