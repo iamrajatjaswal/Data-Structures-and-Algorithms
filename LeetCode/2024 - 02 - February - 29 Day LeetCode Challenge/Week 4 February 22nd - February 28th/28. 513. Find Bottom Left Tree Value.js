5a6ca6d7a3948710b68e32753b00c949570070bc
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-28 16:40:47 +0530

    UPDATE 28. 513. Find Bottom Left Tree Value.js : Added solution with approach 1

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 4 February 22nd - February 28th/28. 513. Find Bottom Left Tree Value.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 4 February 22nd - February 28th/28. 513. Find Bottom Left Tree Value.js
@@ -41,3 +41,38 @@ Output: 7
 **Solution**
 
 */
+/*
+  Appraoch 1
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+
+  const queue = [root];
+  let leftmostValue;
+
+  while (queue.length > 0) {
+      const node = queue.shift();
+
+      leftmostValue = node.val;
+
+      if (node.right) {
+          queue.push(node.right);
+      }
+      if (node.left) {
+          queue.push(node.left);
+      }
+  }
+
+  return leftmostValue;
+};
