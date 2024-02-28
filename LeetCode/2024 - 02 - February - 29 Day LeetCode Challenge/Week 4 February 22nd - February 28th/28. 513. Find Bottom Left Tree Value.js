054eef44df16d1e7fce3ac8d9624588581c2ca33
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-28 16:37:42 +0530

    CREATE 28. 513. Find Bottom Left Tree Value.js : Added problem statement

---
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 4 February 22nd - February 28th/28. 513. Find Bottom Left Tree Value.js
@@ -0,0 +1,43 @@
+/*
+
+Title: 513. Find Bottom Left Tree Value
+URL: https://leetcode.com/problems/diameter-of-binary-tree/
+Difficulty: Easy
+Topics: Tree, Depth-First Search, Binary Tree
+
+
+**Problem**
+
+
+Given the `root` of a binary tree, return the leftmost value in the last row of the tree.
+
+**Example 1:**
+
+!https://assets.leetcode.com/uploads/2020/12/14/tree1.jpg
+
+```
+Input: root = [2,1,3]
+Output: 1
+
+```
+
+**Example 2:**
+
+!https://assets.leetcode.com/uploads/2020/12/14/tree2.jpg
+
+```
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+
+```
+
+**Constraints:**
+
+- The number of nodes in the tree is in the range `[1, 10^4]`.
+- `2^31 <= Node.val <= 2^31 - 1`
+
+
+
+**Solution**
+
+*/
