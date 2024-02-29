c4adf9aadf10bed7c52f7640058c8c20a2985974
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-02-29 16:03:27 +0530

    UPDATE 29. 1609. Even Odd Tree.js : Added solution with approach 1

--- LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 5 February 29th - February 29th/29. 1609. Even Odd Tree.js
+++ LeetCode/2024 - 02 - February - 29 Day LeetCode Challenge/Week 5 February 29th - February 29th/29. 1609. Even Odd Tree.js
@@ -68,3 +68,58 @@ Explanation: Node values in the level 1 should be even integers.
 **Solution**
 
 */
+/*
+  Appraoch 1
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isEvenOddTree = function (root) {
+  if (!root) {
+    return true;
+  }
+
+  let queue = [root];
+  let level = 0;
+
+  while (queue.length > 0) {
+    let size = queue.length;
+    let prevVal =
+      level % 2 === 0 ? Number.MIN_SAFE_INTEGER : Number.MAX_SAFE_INTEGER;
+
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+
+      // Check if the values follow the conditions
+      if (
+        (level % 2 === 0 && (node.val % 2 === 0 || node.val <= prevVal)) ||
+        (level % 2 === 1 && (node.val % 2 === 1 || node.val >= prevVal))
+      ) {
+        return false;
+      }
+
+      prevVal = node.val;
+
+      // Add children to the queue
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    level++;
+  }
+
+  return true;
+};
