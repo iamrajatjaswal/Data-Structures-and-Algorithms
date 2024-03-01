c692e271e602a74c51bc2796e5e5873464a2c3af
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-03-01 17:18:54 +0000

    UPDATE 01. 2864. Maximum Odd Binary Number.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/01. 2864. Maximum Odd Binary Number.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/01. 2864. Maximum Odd Binary Number.js
@@ -46,3 +46,20 @@ Explanation:One of the '1's must be in the last position. The maximum number tha
 **Solution**
 
 */
+/*
+  Appraoch 1
+*/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var maximumOddBinaryNumber = function (s) {
+  let cnt1 = 0,
+    cnt0 = 0;
+  for (let a of s) {
+    if (a === "1") cnt1++;
+    else if (a === "0") cnt0++;
+  }
+  let ans = "1".repeat(cnt1 - 1) + "0".repeat(cnt0) + "1";
+  return ans;
+};
