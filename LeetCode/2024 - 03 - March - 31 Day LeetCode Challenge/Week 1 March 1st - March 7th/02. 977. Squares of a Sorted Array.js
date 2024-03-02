13e21c796797c22408bc2285e5d36860e35a02d7
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-02 07:28:24 +0530

    UPDATE 02. 977. Squares of a Sorted Array.js : Added solution with approach 2

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/02. 977. Squares of a Sorted Array.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/02. 977. Squares of a Sorted Array.js
@@ -103,3 +103,18 @@ function sortedSquares(nums) {
   radixSort(nums);
   return nums;
 }
+
+/*
+  Appraoch 2
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i] * nums[i];
+  }
+  nums.sort((a, b) => a - b);
+  return nums;
+};
