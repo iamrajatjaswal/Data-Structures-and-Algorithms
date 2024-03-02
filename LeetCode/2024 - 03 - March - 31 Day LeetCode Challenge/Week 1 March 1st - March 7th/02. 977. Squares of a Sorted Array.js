5c1d2f6417449a7e86388e9bae92d27ff7f886a0
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-02 07:27:49 +0530

    UPDATE 02. 977. Squares of a Sorted Array.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/02. 977. Squares of a Sorted Array.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/02. 977. Squares of a Sorted Array.js
@@ -49,3 +49,57 @@ solution using a different approach?
 **Solution**
 
 */
+/*
+  Appraoch 1
+*/
+function getMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+function countSort(arr, exp) {
+  const output = new Array(arr.length);
+  const count = new Array(10).fill(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    count[Math.floor(arr[i] / exp) % 10]++;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    output[count[Math.floor(arr[i] / exp) % 10] - 1] = arr[i];
+    count[Math.floor(arr[i] / exp) % 10]--;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = output[i];
+  }
+}
+
+function radixSort(arr) {
+  const max = getMax(arr);
+
+  for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
+    countSort(arr, exp);
+  }
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function sortedSquares(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] *= nums[i];
+  }
+  radixSort(nums);
+  return nums;
+}
