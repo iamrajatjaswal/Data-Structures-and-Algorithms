d7cecf1347168149921348de351761198c2bb7a8
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-03 12:41:01 +0530

    UPDATE 03. 19. Remove Nth Node From End of List.js : added solution for approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/03. 19. Remove Nth Node From End of List.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/03. 19. Remove Nth Node From End of List.js
@@ -47,3 +47,42 @@ Output: [1]
 **Solution**
 
 */
+/*
+  Appraoch 1
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let length = findLength(head);
+  let i = 0,
+    traverseTill = length - n - 1;
+  if (traverseTill === -1) return head.next;
+  let curr = head;
+  while (i < traverseTill) {
+    curr = curr.next;
+    i++;
+  }
+  curr.next = curr.next.next;
+  return head;
+
+  function findLength(head) {
+    let count = 0;
+    if (head === null) return count;
+    let curr = head;
+    while (curr !== null) {
+      count++;
+      curr = curr.next;
+    }
+    return count;
+  }
+};
