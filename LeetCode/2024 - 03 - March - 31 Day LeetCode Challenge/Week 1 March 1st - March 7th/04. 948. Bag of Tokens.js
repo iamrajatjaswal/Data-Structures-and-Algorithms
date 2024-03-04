53de77079bb1ac1b605084c26329a05554f6db20
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-03-04 15:16:28 +0000

    UPDATE 04. 948. Bag of Tokens.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/04. 948. Bag of Tokens.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/04. 948. Bag of Tokens.js
@@ -60,3 +60,35 @@ The maximum score achievable is `2`.
 **Solution**
 
 */
+/*
+  Appraoch 1
+*/
+/**
+ * @param {number[]} tokens
+ * @param {number} power
+ * @return {number}
+ */
+var bagOfTokensScore = function (tokens, power) {
+  tokens.sort((a, b) => a - b);
+  let score = 0;
+  let maxScore = 0;
+  let left = 0;
+  let right = tokens.length - 1;
+
+  while (left <= right) {
+    if (power >= tokens[left]) {
+      power -= tokens[left];
+      score++;
+      left++;
+      maxScore = Math.max(maxScore, score);
+    } else if (score > 0) {
+      power += tokens[right];
+      score--;
+      right--;
+    } else {
+      break;
+    }
+  }
+
+  return maxScore;
+};
