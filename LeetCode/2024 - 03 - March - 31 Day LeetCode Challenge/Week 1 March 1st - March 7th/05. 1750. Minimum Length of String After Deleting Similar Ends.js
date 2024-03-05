8add74ba97ffe0972eeed09b777b95ad48dadabf
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-03-05 15:11:21 +0000

    UPDATE 05. 1750. Minimum Length of String After Deleting Similar Ends.js : Added solution for approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/05. 1750. Minimum Length of String After Deleting Similar Ends.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/05. 1750. Minimum Length of String After Deleting Similar Ends.js
@@ -63,3 +63,17 @@ Explanation: An optimal sequence of operations is:
 /*
   Approach 1
 */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumLength = function (s) {
+  let l = 0,
+    r = s.length - 1;
+  while (l < r && s[l] === s[r]) {
+    let ch = s[l];
+    while (l <= r && s[l] === ch) l++;
+    while (l <= r && s[r] === ch) r--;
+  }
+  return r - l + 1;
+};
