8a24b762310bd0d7c4734fa72c284907c1f98373
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-06 19:04:58 +0530

    UPDATE 06. 141. Linked List Cycle.js : added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/06. 141. Linked List Cycle.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 1 March 1st - March 7th/06. 141. Linked List Cycle.js
@@ -61,3 +61,30 @@ Explanation: There is no cycle in the linked list.
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let visited_nodes = new Set();
+  let current_node = head;
+  while (current_node !== null) {
+    if (visited_nodes.has(current_node)) {
+      return true;
+    }
+    visited_nodes.add(current_node);
+    current_node = current_node.next;
+  }
+  return false;
+};
