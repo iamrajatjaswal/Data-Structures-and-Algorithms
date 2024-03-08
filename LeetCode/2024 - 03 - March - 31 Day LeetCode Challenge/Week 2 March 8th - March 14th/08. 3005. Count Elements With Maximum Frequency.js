6a1b64966e48087f2acda871eb42277acbec7c0d
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-08 18:43:44 +0530

    UPDATE 08. 3005. Count Elements With Maximum Frequency.js : Added solution for approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/08. 3005. Count Elements With Maximum Frequency.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/08. 3005. Count Elements With Maximum Frequency.js
@@ -45,3 +45,26 @@ So the number of elements in the array with maximum frequency is 5.
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxFrequencyElements = function (nums) {
+  const freqCounter = new Map();
+  for (const num of nums) {
+    freqCounter.set(num, (freqCounter.get(num) || 0) + 1);
+  }
+
+  const maxFrequency = Math.max(...freqCounter.values());
+
+  const maxFreqElements = [...freqCounter.keys()].filter(
+    (num) => freqCounter.get(num) === maxFrequency
+  );
+
+  const totalFrequency = maxFrequency * maxFreqElements.length;
+
+  return totalFrequency;
+};
