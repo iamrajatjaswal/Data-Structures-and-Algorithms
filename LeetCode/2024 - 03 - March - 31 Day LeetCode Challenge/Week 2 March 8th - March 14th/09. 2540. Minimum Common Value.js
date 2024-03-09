72f060ce46496151ffe0dfeacdb3f55df29b8dcc
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-09 23:20:20 +0530

    UPDATE 09. 2540. Minimum Common Value.js : Added solutionw with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/09. 2540. Minimum Common Value.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/09. 2540. Minimum Common Value.js
@@ -42,3 +42,29 @@ Explanation: There are two common elements in the array 2 and 3 out of which 2 i
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function (nums1, nums2) {
+  let common = Infinity;
+  let i = 0,
+    j = 0;
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      common = nums1[i];
+      break;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return common !== Infinity ? common : -1;
+};
