54bcb5e7e6a06f64a02a60e6f61d200d23684e68
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-10 20:08:41 +0530

    UPDATE 10. 349. Intersection of Two Arrays.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/10. 349. Intersection of Two Arrays.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/10. 349. Intersection of Two Arrays.js
@@ -38,3 +38,27 @@ Explanation: [4,9] is also accepted.
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  const mp = {};
+  for (const num of nums1) {
+    mp[num] = (mp[num] || 0) + 1;
+  }
+
+  const result = [];
+  for (const num of nums2) {
+    if (mp[num]) {
+      result.push(num);
+      delete mp[num];
+    }
+  }
+
+  return result;
+};
