fa623961280eceea6581d79f4f1f77564613c056
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-11 09:31:28 +0530

    UPDATE 11. 791. Custom Sort String.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/11. 791. Custom Sort String.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/11. 791. Custom Sort String.js
@@ -46,3 +46,28 @@ Following the order of appearance in `order`, `"b"`, `"c"`, and `"a"` from�
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
+var customSortString = function (order, s) {
+  let result = "";
+  let mp = new Map();
+  for (let char of s) {
+    mp.set(char, (mp.get(char) || 0) + 1);
+  }
+  for (let char of order) {
+    if (mp.has(char)) {
+      result += char.repeat(mp.get(char));
+      mp.delete(char);
+    }
+  }
+  for (let [char, count] of mp.entries()) {
+    result += char.repeat(count);
+  }
+  return result;
+};
