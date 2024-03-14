12a9283991b36addfe76116eaa1e442c45a15b67
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-14 16:36:31 +0530

    UPDATE 14. 930. Binary Subarrays With Sum.js : Added problem solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/14. 930. Binary Subarrays With Sum.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 2 March 8th - March 14th/14. 930. Binary Subarrays With Sum.js
@@ -47,3 +47,37 @@ Output: 15
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} goal
+ * @return {number}
+ */
+var numSubarraysWithSum = function (nums, goal) {
+  let i = 0,
+    count = 0,
+    res = 0;
+  for (let j = 0; j < nums.length; ++j) {
+    if (nums[j] === 1) {
+      goal--;
+      count = 0;
+    }
+
+    while (goal === 0 && i <= j) {
+      goal += nums[i];
+      i++;
+      count++;
+      if (i > j - goal + 1) break;
+    }
+
+    while (goal < 0) {
+      goal += nums[i];
+      i++;
+    }
+
+    res += count;
+  }
+  return res;
+};
