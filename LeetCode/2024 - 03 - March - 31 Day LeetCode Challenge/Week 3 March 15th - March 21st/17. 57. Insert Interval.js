1545fcf6e3b631a77ceb8e84d15b37e01815c221
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-17 09:53:45 +0530

    UPDATE 17. 57. Insert Interval.js : Added problem solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/17. 57. Insert Interval.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/17. 57. Insert Interval.js
@@ -48,3 +48,39 @@ Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (intervals, newInterval) {
+  const result = [];
+
+  // Iterate through intervals and add non-overlapping intervals before newInterval
+  let i = 0;
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  // Merge overlapping intervals
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+
+  // Add merged newInterval
+  result.push(newInterval);
+
+  // Add non-overlapping intervals after newInterval
+  while (i < intervals.length) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  return result;
+};
