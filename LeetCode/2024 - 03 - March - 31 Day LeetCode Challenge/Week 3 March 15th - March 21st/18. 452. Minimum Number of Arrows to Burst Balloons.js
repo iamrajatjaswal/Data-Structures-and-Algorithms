73b295269d939eacbb9ed12df065c2cb80ebdb8a
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-03-18 17:27:22 +0000

    UPDATE 18. 452. Minimum Number of Arrows to Burst Balloons.js : Added solution with Approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/18. 452. Minimum Number of Arrows to Burst Balloons.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/18. 452. Minimum Number of Arrows to Burst Balloons.js
@@ -58,3 +58,27 @@ Explanation: The balloons can be burst by 2 arrows:
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function (points) {
+  // Sort the balloons based on their end coordinates
+  points.sort((a, b) => a[1] - b[1]);
+
+  let arrows = 1;
+  let prevEnd = points[0][1];
+
+  // Count the number of non-overlapping intervals
+  for (let i = 1; i < points.length; ++i) {
+    if (points[i][0] > prevEnd) {
+      arrows++;
+      prevEnd = points[i][1];
+    }
+  }
+
+  return arrows;
+};
