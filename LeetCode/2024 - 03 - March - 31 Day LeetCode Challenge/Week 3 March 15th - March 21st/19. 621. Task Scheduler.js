58cc25a05116fd340702351b77626664959c3444
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-03-19 16:41:51 +0000

    UPDATE 19. 621. Task Scheduler.js : Added problem solution wiith Approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/19. 621. Task Scheduler.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/19. 621. Task Scheduler.js
@@ -56,3 +56,26 @@ separated by 3 intervals. This leads to idling twice between repetitions
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function (tasks, n) {
+  let freq = Array(26).fill(0);
+  for (let task of tasks) {
+    freq[task.charCodeAt(0) - "A".charCodeAt(0)]++;
+  }
+  freq.sort((a, b) => b - a);
+  let chunk = freq[0] - 1;
+  let idle = chunk * n;
+
+  for (let i = 1; i < 26; i++) {
+    idle -= Math.min(chunk, freq[i]);
+  }
+
+  return idle < 0 ? tasks.length : tasks.length + idle;
+};
