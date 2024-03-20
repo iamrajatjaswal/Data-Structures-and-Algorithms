faada5bae5ad007d19ec66fa4780d886b5820946
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-20 17:52:52 +0530

    UPDATE 20. 1669. Merge In Between Linked Lists.js : Added problem solution with Approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/20. 1669. Merge In Between Linked Lists.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/20. 1669. Merge In Between Linked Lists.js
@@ -51,3 +51,52 @@ Explanation: The blue edges and nodes in the above figure indicate the result.
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeInBetween = function (list1, a, b, list2) {
+  let prev = list1,
+    temp1 = list1,
+    curr = list1;
+  a--; // Adjust a to match zero-based indexing
+
+  // Traverse list1 to find the node at position (a-1)
+  while (temp1 && a-- > 0) {
+    temp1 = temp1.next;
+    prev = temp1;
+  }
+
+  temp1 = list1; // Reset temp1 to traverse list1 again
+
+  // Traverse list1 to find the node at position (b+1)
+  while (temp1 && b-- > 0) {
+    temp1 = temp1.next;
+    curr = temp1;
+  }
+
+  let temp2 = list2; // Initialize pointer to traverse list2
+
+  // Traverse list2 to find the last node
+  while (temp2.next !== null) {
+    temp2 = temp2.next;
+  }
+
+  prev.next = list2; // Connect the node before position 'a' to the head of list2
+  temp2.next = curr.next; // Connect the last node of list2 to the node after position 'b'
+
+  return list1; // Return the head of list1
+};
