cf82735ba6ff71c99aeb9d6184dfd388095632f3
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-24 07:59:14 +0530

    UPDATE 24. 287. Find the Duplicate Number.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/24. 287. Find the Duplicate Number.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 3 March 15th - March 21st/24. 287. Find the Duplicate Number.js
@@ -54,3 +54,20 @@ Output: 3
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  const seen = new Set();
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return num;
+    }
+    seen.add(num);
+  }
+  return -1; // Just to satisfy the compiler, this should never be reached
+};
