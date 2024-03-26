4c9cd71ea00ea6af370338584553554b185dcdab
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-26 08:03:16 +0530

    UPDATE 26. 41. First Missing Positive.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 4 March 22nd - March 28th/26. 41. First Missing Positive.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 4 March 22nd - March 28th/26. 41. First Missing Positive.js
@@ -49,3 +49,23 @@ Explanation: The smallest positive integer 1 is missing.
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+  const mp = new Map();
+  let maxi = Math.max(...nums, 0);
+  for (let num of nums) {
+    mp.set(num, true);
+  }
+  for (let i = 1; i < maxi; i++) {
+    if (!mp.has(i)) {
+      return i;
+    }
+  }
+  return maxi < 0 ? 1 : maxi + 1;
+};
