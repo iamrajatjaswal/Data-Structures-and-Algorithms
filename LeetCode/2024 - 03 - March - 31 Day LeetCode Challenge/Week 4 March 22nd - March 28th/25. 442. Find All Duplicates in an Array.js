3e1e3d08204c16a728c33e5bfa11354238bde45a
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-26 07:50:14 +0530

    UPDATE 25. 442. Find All Duplicates in an Array.js : Added solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 4 March 22nd - March 28th/25. 442. Find All Duplicates in an Array.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 4 March 22nd - March 28th/25. 442. Find All Duplicates in an Array.js
@@ -49,3 +49,24 @@ Output: []
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+  const seen = new Set();
+  const duplicates = [];
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      duplicates.push(num);
+    } else {
+      seen.add(num);
+    }
+  }
+
+  return duplicates;
+};
