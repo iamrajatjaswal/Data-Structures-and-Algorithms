2819a06addba7a105f0f18429062111b46d95043
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-28 10:18:21 +0530

    UPDATE 28. 2958. Length of Longest Subarray With at Most K Frequency.js : Added solution with Approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 4 March 22nd - March 28th/28. 2958. Length of Longest Subarray With at Most K Frequency.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 4 March 22nd - March 28th/28. 2958. Length of Longest Subarray With at Most K Frequency.js
@@ -60,3 +60,39 @@ It can be shown that there are no good subarrays with length more than 4.
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarrayLength = function (nums, k) {
+  let start = 0;
+  let end = 0;
+  const length = nums.length;
+  const frequencyMap = {};
+
+  let maxLength = 0;
+
+  for (end = 0; end < length; end++) {
+    const current = nums[end];
+    frequencyMap[current] = (frequencyMap[current] || 0) + 1;
+
+    if (frequencyMap[current] > k) {
+      while (start < end && frequencyMap[current] > k) {
+        if (frequencyMap[nums[start]] === 1) {
+          delete frequencyMap[nums[start]];
+        } else {
+          frequencyMap[nums[start]]--;
+        }
+        start++;
+      }
+    }
+
+    maxLength = Math.max(maxLength, end - start + 1);
+  }
+
+  return maxLength;
+};
