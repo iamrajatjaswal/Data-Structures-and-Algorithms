e89a45f2d4fbc34f6d21c12564ee8da7d593844b
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-03-31 09:22:25 +0530

    UPDATE 31. 2444. Count Subarrays With Fixed Bounds.js : Added problem solution with approach 1

--- LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 5 March 29th - March 31st/31. 2444. Count Subarrays With Fixed Bounds.js
+++ LeetCode/2024 - 03 - March - 31 Day LeetCode Challenge/Week 5 March 29th - March 31st/31. 2444. Count Subarrays With Fixed Bounds.js
@@ -48,3 +48,26 @@ Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 poss
 **Solution**
 
 */
+/*
+  Approach 1
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} minK
+ * @param {number} maxK
+ * @return {number}
+ */
+var countSubarrays = function (nums, minK, maxK) {
+  let res = 0,
+    jbad = -1,
+    jmin = -1,
+    jmax = -1,
+    n = nums.length;
+  for (let i = 0; i < n; ++i) {
+    if (nums[i] < minK || nums[i] > maxK) jbad = i;
+    if (nums[i] === minK) jmin = i;
+    if (nums[i] === maxK) jmax = i;
+    res += Math.max(0, Math.min(jmin, jmax) - jbad);
+  }
+  return res;
+};
