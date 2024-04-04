c04481963c023d399a6da2fc7a96aaa78feaf72e
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-04 12:08:00 +0530

    CREATE 04. 1614. Maximum Nesting Depth of the Parentheses.js : Added problem statement

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 1 April 1st - April 7th/04. 1614. Maximum Nesting Depth of the Parentheses.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 1 April 1st - April 7th/04. 1614. Maximum Nesting Depth of the Parentheses.js
@@ -54,32 +54,3 @@ Output: 3
 **Solution**
 
 */
-/*
-  Appraoch 1
-
-  **Time complexity:**
-
-    O(n), where n is the length of the input string `s`. We traverse the entire string once.
-
-  **Space complexity:**
-
-    O(1), as we only use a constant amount of extra space for variables `count` and `max_num`.
-    
-*/
-/**
- * @param {string} s
- * @return {number}
- */
-var maxDepth = function (s) {
-  let count = 0;
-  let maxNum = 0;
-  for (let c of s) {
-    if (c === "(") {
-      count++;
-      if (maxNum < count) maxNum = count;
-    } else if (c === ")") {
-      count--;
-    }
-  }
-  return maxNum;
-};
