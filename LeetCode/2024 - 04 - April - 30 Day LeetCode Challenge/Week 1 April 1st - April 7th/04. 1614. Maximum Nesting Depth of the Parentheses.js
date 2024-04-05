bdf523642405a0bf47d5efb55373e20ed896a78d
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-05 13:47:45 +0530

    UPDATE 04. 1614. Maximum Nesting Depth of the Parentheses.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 1 April 1st - April 7th/04. 1614. Maximum Nesting Depth of the Parentheses.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 1 April 1st - April 7th/04. 1614. Maximum Nesting Depth of the Parentheses.js
@@ -60,3 +60,27 @@ Output: "s"
 **Solution**
 
 */
+/*
+  Approach 1 :: "stack-based approach”
+
+*/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function (s) {
+  const stack = [];
+
+  for (const char of s) {
+    if (
+      stack.length > 0 &&
+      Math.abs(char.charCodeAt() - stack[stack.length - 1].charCodeAt()) === 32
+    ) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.join("");
+};
