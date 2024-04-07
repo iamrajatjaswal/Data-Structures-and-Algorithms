1c86b463b4db35d24197c5f111a9f72aec001170
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-07 15:53:43 +0530

    CREATE 07. 678. Valid Parenthesis String.js : Added problem statement

---
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 1 April 1st - April 7th/07. 678. Valid Parenthesis String.js
@@ -0,0 +1,54 @@
+/*
+
+Title: 678. Valid Parenthesis String
+URL: https://leetcode.com/problems/valid-parenthesis-string/
+Difficulty: Medium
+Topics: String, Dynamic Programming, Stack, Greedy
+
+
+**Problem**
+
+
+Given a string `s` containing only three types of characters: `'('`, `')'` and `'*'`, return `true` *if* `s` *is **valid***.
+
+The following rules define a **valid** string:
+
+- Any left parenthesis `'('` must have a corresponding right parenthesis `')'`.
+- Any right parenthesis `')'` must have a corresponding left parenthesis `'('`.
+- Left parenthesis `'('` must go before the corresponding right parenthesis `')'`.
+- `'*'` could be treated as a single right parenthesis `')'` or a single left parenthesis `'('` or an empty string `""`.
+
+**Example 1:**
+
+```
+Input: s = "()"
+Output: true
+
+```
+
+**Example 2:**
+
+```
+Input: s = "(*)"
+Output: true
+
+```
+
+**Example 3:**
+
+```
+Input: s = "(*))"
+Output: true
+
+```
+
+**Constraints:**
+
+- `1 <= s.length <= 100`
+- `s[i]` is `'('`, `')'` or `'*'`.
+
+
+
+**Solution**
+
+*/
