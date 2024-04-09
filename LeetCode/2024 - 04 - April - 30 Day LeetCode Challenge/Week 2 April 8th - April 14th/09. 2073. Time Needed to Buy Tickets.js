21d47a01bd3f23621b6eb1104abed7e1c1c7779c
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-09 15:38:52 +0530

    UPDATE 09. 2073. Time Needed to Buy Tickets.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 2 April 8th - April 14th/09. 2073. Time Needed to Buy Tickets.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 2 April 8th - April 14th/09. 2073. Time Needed to Buy Tickets.js
@@ -53,3 +53,24 @@ The person at position 0 has successfully bought 5 tickets and it took 4 + 1 + 
 **Solution**
 
 */
+/*
+  Approach 1 :: "Simulation" :: T->O(N) : S->O(1)
+*/
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function (tickets, k) {
+  let total = 0;
+
+  for (let i = 0; i < tickets.length; i++) {
+    if (i <= k) {
+      total += Math.min(tickets[i], tickets[k]);
+    } else {
+      total += Math.min(tickets[i], tickets[k] - 1);
+    }
+  }
+
+  return total;
+};
