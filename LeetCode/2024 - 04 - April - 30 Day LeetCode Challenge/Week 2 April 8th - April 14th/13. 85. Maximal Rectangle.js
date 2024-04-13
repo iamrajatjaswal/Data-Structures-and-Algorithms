64e3b4acbfc1f876767ebdab6a71cccdbdb3f2a7
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-13 21:52:20 +0530

    UPDATE 13. 85. Maximal Rectangle.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 2 April 8th - April 14th/13. 85. Maximal Rectangle.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 2 April 8th - April 14th/13. 85. Maximal Rectangle.js
@@ -50,3 +50,48 @@ Output: 1
 **Solution**
 
 */
+/*
+  Approach 1 :: "Histogram Approach"
+*/
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function (matrix) {
+  if (matrix.length === 0) return 0;
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let left = new Array(n).fill(0);
+  let right = new Array(n).fill(n);
+  let height = new Array(n).fill(0);
+  let maxA = 0;
+  for (let i = 0; i < m; i++) {
+    let cur_left = 0,
+      cur_right = n;
+    for (let j = 0; j < n; j++) {
+      // compute height (can do this from either side)
+      if (matrix[i][j] === "1") height[j]++;
+      else height[j] = 0;
+    }
+    for (let j = 0; j < n; j++) {
+      // compute left (from left to right)
+      if (matrix[i][j] === "1") left[j] = Math.max(left[j], cur_left);
+      else {
+        left[j] = 0;
+        cur_left = j + 1;
+      }
+    }
+    // compute right (from right to left)
+    for (let j = n - 1; j >= 0; j--) {
+      if (matrix[i][j] === "1") right[j] = Math.min(right[j], cur_right);
+      else {
+        right[j] = n;
+        cur_right = j;
+      }
+    }
+    // compute the area of rectangle (can do this from either side)
+    for (let j = 0; j < n; j++)
+      maxA = Math.max(maxA, (right[j] - left[j]) * height[j]);
+  }
+  return maxA;
+};
