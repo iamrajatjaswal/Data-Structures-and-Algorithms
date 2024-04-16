d74a95f5961e4299c7b08e995948dfac71e05596
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-16 16:05:59 +0530

    UPDATE 16. 623. Add One Row to Tree.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/16. 623. Add One Row to Tree.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/16. 623. Add One Row to Tree.js
@@ -52,3 +52,55 @@ Output: [4,2,null,1,1,3,null,null,1]
 **Solution**
 
 */
+/*
+  Approach 1 :: Depth-first Traversal”  
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
+var addOneRow = function (root, val, depth) {
+  const helper = (node, val, depth, currdepth) => {
+    if (depth === 1) {
+      const newRoot = new TreeNode(val);
+      newRoot.left = node;
+      return newRoot;
+    }
+
+    if (!node) {
+      return null;
+    }
+
+    if (currdepth === depth - 1) {
+      const leftman = node.left;
+      const rightman = node.right;
+
+      node.left = new TreeNode(val);
+      node.left.left = leftman;
+      node.left.right = null;
+
+      node.right = new TreeNode(val);
+      node.right.left = null;
+      node.right.right = rightman;
+
+      return node;
+    }
+
+    node.left = helper(node.left, val, depth, currdepth + 1);
+    node.right = helper(node.right, val, depth, currdepth + 1);
+
+    return node;
+  };
+
+  return helper(root, val, depth, 1);
+};
