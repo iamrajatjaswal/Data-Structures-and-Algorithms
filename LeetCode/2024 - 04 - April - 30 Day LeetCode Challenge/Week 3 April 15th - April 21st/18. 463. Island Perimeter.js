a1d1b32e9e6bb71de18460de9fcabc8c4267991b
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-18 08:19:04 +0530

    UPDATE 18. 463. Island Perimeter.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/18. 463. Island Perimeter.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/18. 463. Island Perimeter.js
@@ -57,3 +57,29 @@ Output: 4
 **Solution**
 
 */
+/*
+  Approach 1 :: Depth-first Traversal”
+*/
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function (grid) {
+  let n = grid.length;
+  let m = grid[0].length;
+  let cnt = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (grid[i][j] === 1) {
+        if ((j > 0 && grid[i][j - 1] === 0) || j === 0) cnt++;
+
+        if ((i > 0 && grid[i - 1][j] === 0) || i === 0) cnt++;
+
+        if ((j < m - 1 && grid[i][j + 1] === 0) || j === m - 1) cnt++;
+
+        if ((i < n - 1 && grid[i + 1][j] === 0) || i === n - 1) cnt++;
+      }
+    }
+  }
+  return cnt;
+};
