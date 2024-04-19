45768aa40c6f16d4eb46ada7d3d63f20ef684688
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-19 17:14:20 +0530

    UPDATE 19. 200. Number of Islands.js : Added solutoin with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/19. 200. Number of Islands.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/19. 200. Number of Islands.js
@@ -15,3 +15,44 @@ Topics: Array, Depth-First Search, Breadth-First Search, Union Find, Matrix
 **Solution**
 
 */
+/*
+  Approach 1 :: Breadth-first Traversal”
+*/
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  if (!grid.length || !grid[0].length) return 0;
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let islands = 0;
+
+  const dfs = (row, col) => {
+    if (
+      row < 0 ||
+      col < 0 ||
+      row >= rows ||
+      col >= cols ||
+      grid[row][col] !== "1"
+    )
+      return;
+    grid[row][col] = "0";
+    dfs(row - 1, col);
+    dfs(row + 1, col);
+    dfs(row, col - 1);
+    dfs(row, col + 1);
+  };
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (grid[row][col] === "1") {
+        dfs(row, col);
+        islands++;
+      }
+    }
+  }
+
+  return islands;
+};
