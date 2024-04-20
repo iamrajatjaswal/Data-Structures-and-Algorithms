dc2b6bda9f5ef453f1dd7e8259125d07733de835
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-20 12:56:26 +0530

    UPDATE 20. 1992. Find All Groups of Farmland.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/20. 1992. Find All Groups of Farmland.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/20. 1992. Find All Groups of Farmland.js
@@ -66,3 +66,44 @@ There are no groups of farmland.
 **Solution**
 
 */
+/*
+  Approach 1 :: Depth-first Traversal”
+*/
+/**
+ * @param {number[][]} land
+ * @return {number[][]}
+ */
+var findFarmland = function (land) {
+  const result = [];
+  const m = land.length;
+  const n = land[0].length;
+
+  const findFarmlandCoordinates = (row, col) => {
+    const coordinates = [row, col];
+    let r = row,
+      c = col;
+
+    while (r < m && land[r][col] === 1) r++;
+    while (c < n && land[row][c] === 1) c++;
+
+    coordinates.push(r - 1, c - 1);
+
+    for (let i = row; i <= r - 1; i++) {
+      for (let j = col; j <= c - 1; j++) {
+        land[i][j] = 0;
+      }
+    }
+
+    return coordinates;
+  };
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (land[i][j] === 1) {
+        result.push(findFarmlandCoordinates(i, j));
+      }
+    }
+  }
+
+  return result;
+};
