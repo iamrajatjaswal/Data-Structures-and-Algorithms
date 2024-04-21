88994c367a3b3e0eefba22725d8e3812beb4cc3b
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-21 23:28:07 +0530

    UPDATE 21. 1971. Find if Path Exists in Graph.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/21. 1971. Find if Path Exists in Graph.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 3 April 15th - April 21st/21. 1971. Find if Path Exists in Graph.js
@@ -54,3 +54,32 @@ Explanation: There is no path from vertex 0 to vertex 5.
 **Solution**
 
 */
+/*
+  Approach 1 :: Depth-first Traversal”
+
+*/
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function (n, edges, source, destination) {
+  if (n === 1) return true; // Special case where there is only one vertex
+  let visited = new Array(n).fill(false);
+  visited[source] = true;
+  let flag = true;
+  while (flag) {
+    flag = false;
+    for (const edge of edges) {
+      if (visited[edge[0]] !== visited[edge[1]]) {
+        visited[edge[0]] = true;
+        visited[edge[1]] = true;
+        flag = true;
+      }
+      if (visited[destination]) return true;
+    }
+  }
+  return false;
+};
