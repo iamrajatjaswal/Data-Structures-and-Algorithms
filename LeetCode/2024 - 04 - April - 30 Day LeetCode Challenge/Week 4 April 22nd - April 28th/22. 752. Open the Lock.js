3295a9a71686d8a4c2d8fa8a042616ff86bc9aa7
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-22 17:54:53 +0530

    UPDATE 22. 752. Open the Lock.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 4 April 22nd - April 28th/22. 752. Open the Lock.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 4 April 22nd - April 28th/22. 752. Open the Lock.js
@@ -59,3 +59,58 @@ Explanation: We cannot reach the target without getting stuck.
 **Solution**
 
 */
+/*
+  Approach 1 :: Breadth-first Traversal”
+
+*/
+/**
+ * @param {string[]} deadends
+ * @param {string} target
+ * @return {number}
+ */
+var openLock = function (deadends, target) {
+  const pow10 = [1, 10, 100, 1000];
+  const visit = new Array(10000).fill(0); // 0: not visited, 1: visited through forward direction, -1: visited through backward direction, 2: deadends
+  deadends.forEach((dead) => {
+    visit[parseInt(dead)] = 2;
+  });
+  const src = 0,
+    dest = parseInt(target);
+  let steps = 0,
+    dir = 1;
+  if (visit[src] == 2 || visit[dest] == 2) return -1;
+  if (src == dest) return 0;
+  let forward = [],
+    backward = [];
+
+  forward.push(src);
+  visit[src] = 1;
+  backward.push(dest);
+  visit[dest] = -1;
+  while (forward.length > 0 && backward.length > 0) {
+    if (forward.length > backward.length) {
+      [forward, backward] = [backward, forward];
+      dir = -dir;
+    }
+    steps++;
+    const size = forward.length;
+    for (let j = 0; j < size; j++) {
+      const cur = forward.shift();
+      for (let k = 0; k < 4; k++) {
+        const p = pow10[k];
+        const d = Math.floor(cur / p) % 10;
+        for (let i = -1; i <= 1; i += 2) {
+          let z = d + i;
+          z = z === -1 ? 9 : z === 10 ? 0 : z;
+          const next = cur + (z - d) * p;
+          if (visit[next] == -dir) return steps;
+          if (visit[next] == 0) {
+            forward.push(next);
+            visit[next] = dir;
+          }
+        }
+      }
+    }
+  }
+  return -1;
+};
