fdeb90f496696c484e8ba4a7f9fc97375605e250
Rajat Jaswal <44950610+iamrajatjaswal@users.noreply.github.com>
2024-04-24 13:31:46 +0000

    UPDATE 24. 1137. N-th Tribonacci Number.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 4 April 22nd - April 28th/24. 1137. N-th Tribonacci Number.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 4 April 22nd - April 28th/24. 1137. N-th Tribonacci Number.js
@@ -43,3 +43,24 @@ Output: 1389537
 **Solution**
 
 */
+/*
+  Approach 1 :: "Dynamic Programming" approach
+*/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function (n) {
+  if (n < 2) return n;
+
+  let dp = [0, 1, 1];
+
+  for (let i = 3; i <= n; ++i) {
+    let next = dp[0] + dp[1] + dp[2];
+    dp[0] = dp[1];
+    dp[1] = dp[2];
+    dp[2] = next;
+  }
+
+  return dp[2];
+};
