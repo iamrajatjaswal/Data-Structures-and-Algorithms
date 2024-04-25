b89e4d9ca663bc89abe8b78bfffe06ac67bfccc8
Rajat Jaswal <iamrajatjaswal@gmail.com>
2024-04-25 06:29:10 +0530

    UPDATE 25. 2370. Longest Ideal Subsequence.js : Added solution with approach 1

--- LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 4 April 22nd - April 28th/25. 2370. Longest Ideal Subsequence.js
+++ LeetCode/2024 - 04 - April - 30 Day LeetCode Challenge/Week 4 April 22nd - April 28th/25. 2370. Longest Ideal Subsequence.js
@@ -48,3 +48,34 @@ Explanation: The longest ideal string is "abcd". The length of this string is 4,
 **Solution**
 
 */
+/*
+  Approach 1 :: "Dynamic Programming" approach :: T→O(26 * n) : S→O(1)
+*/
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var longestIdealString = function (s, k) {
+  const rangePerOrd = Array.from({ length: 26 }, (_, x) => ({
+    start: Math.max(0, x - k),
+    end: Math.min(26, x + k + 1),
+  }));
+
+  const bestPrev = new Array(26).fill(0);
+  const ordA = "a".charCodeAt(0);
+
+  for (const ch of s) {
+    const idx = ch.charCodeAt(0) - ordA;
+    const { start, end } = rangePerOrd[idx];
+    let bestCurrent = 0;
+
+    for (let j = start; j < end; j++) {
+      bestCurrent = Math.max(bestCurrent, bestPrev[j]);
+    }
+
+    bestPrev[idx] = bestCurrent + 1;
+  }
+
+  return Math.max(...bestPrev);
+};
