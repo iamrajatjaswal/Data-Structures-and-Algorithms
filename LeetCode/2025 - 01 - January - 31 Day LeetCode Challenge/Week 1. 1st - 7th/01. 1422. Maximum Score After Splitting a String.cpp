f77398d440fc22309a8de3d5dbe872f3efcf8f22
Rajat Jaswal <iamrajatjaswal@gmail.com>
2025-01-02 00:00:06 +0530

    CREATE LeetCode/2025 - 01 - January - 31 Day LeetCode Challenge/Week 1. 1st - 7th/01. 1422. Maximum Score After Splitting a String.cpp

--- LeetCode/2025 - 01 - January - 31 Day LeetCode Challenge/Week 1. 1st - 7th/01. 1422. Maximum Score After Splitting a String.cpp
+++ LeetCode/2025 - 01 - January - 31 Day LeetCode Challenge/Week 1. 1st - 7th/01. 1422. Maximum Score After Splitting a String.cpp
@@ -54,3 +54,38 @@ Output: 3
 **Solution**
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxScore(string s) {
+  int n = s.size();
+  vector<int> leftZeroCount(n, 0), rightOneCount(n, 0);
+
+  // Count zeros from the left
+  leftZeroCount[0] = (s[0] == '0') ? 1 : 0;
+  for (int i = 1; i < n; i++) {
+    leftZeroCount[i] = leftZeroCount[i - 1] + (s[i] == '0' ? 1 : 0);
+  }
+
+  // Count ones from the right
+  rightOneCount[n - 1] = (s[n - 1] == '1') ? 1 : 0;
+  for (int i = n - 2; i >= 0; i--) {
+    rightOneCount[i] = rightOneCount[i + 1] + (s[i] == '1' ? 1 : 0);
+  }
+
+  // Calculate maximum score by splitting
+  int maxScore = 0;
+  for (int i = 0; i < n - 1; i++) { // Split before the last character
+    int score = leftZeroCount[i] + rightOneCount[i + 1];
+    maxScore = max(maxScore, score);
+  }
+
+  return maxScore;
+}
+
+int main() {
+  // maxScore();
+
+  return 0;
+}
