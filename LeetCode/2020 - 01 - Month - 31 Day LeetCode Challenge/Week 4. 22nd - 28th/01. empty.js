93069ef3ab66f6ac108926a9d089672d92680b66
Rajat Jaswal <iamrajatjaswal@gmail.com>
2025-01-01 23:32:57 +0530

    CREATE boilerplate folder structure for monthly daily leetcode questions

---
+++ LeetCode/2020 - 01 - Month - 31 Day LeetCode Challenge/Week 4. 22nd - 28th/01. empty.js
@@ -0,0 +1,86 @@
+/*
+
+Title: 58. Length of Last Word
+URL: https://leetcode.com/problems/length-of-last-word/
+Difficulty: Easy
+Topics: String
+
+
+**Problem**
+
+
+Given a string `s` consisting of words and spaces, return *the length of the **last** word in the string.* A **word** is a maximal substring consisting of non-space characters only.
+
+**Example 1:**
+
+```
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+```
+
+**Example 2:**
+
+```
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+```
+
+**Example 3:**
+
+```
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+```
+
+**Constraints:**
+
+- `1 <= s.length <= 10^4`
+- `s` consists of only English letters and spaces `' '`.
+- There will be at least one word in `s`.
+
+
+
+**Solution**
+
+*/
+/*
+  Appraoch 1 :: Linear Scan / Linear Traversal :: T->O(N) : S->O(1)
+
+  - Time complexity:
+    
+    The time complexity of this approach is O(n), where n is the length of the input string. We iterate through the string once to determine the length of the last word.
+    
+  - Space complexity:
+    
+    The space complexity of this approach is O(1) since we are using a constant amount of additional space to store the count and the space flag, regardless of the size of the input string.
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let count = 0; // Variable to store the length of the last word
+  let space = false; // Boolean variable to track if a space is encountered
+
+  // Iterate through each character in the string
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      // If a space character is encountered
+      space = true;
+    } else if (s[i] !== " " && space) {
+      // If a non-space character is encountered after a space
+      count = 1; // Start counting a new word
+      space = false;
+    } else {
+      count++; // Increment the count for non-space characters
+    }
+  }
+
+  return count;
+};
